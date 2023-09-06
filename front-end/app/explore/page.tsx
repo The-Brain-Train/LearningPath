5ba5cb78dad2c854140478874e4fc1fa9b6606d7
Monatm <mona.talebian@gmail.com>
2023-09-06 11:13:15 +0200

    feat: adding searchhistory

--- front-end/app/explore/page.tsx
+++ front-end/app/explore/page.tsx
@@ -2,11 +2,13 @@ import Paper from "@mui/material/Paper";
 import InputBase from "@mui/material/InputBase";
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
+import { enhancedDummyDataTwo } from "../dummyData";
+import { Key, ReactElement, JSXElementConstructor, ReactNode, PromiseLikeOfReactNode, ReactPortal } from "react";
 
 export default function Explore() {
   return (
-    <main className="flex items-center justify-center">
-      <Paper 
+    <main className="flex items-center justify-center flex-col">
+      <Paper
         component="form"
         sx={{ m: "20px", p: "2px 4px", display: "flex", alignItems: "center", width: 250 }}
       >
@@ -19,6 +21,15 @@ export default function Explore() {
           <SearchIcon />
         </IconButton>
       </Paper>
+      <div className="text-center">
+        <ul>
+          {enhancedDummyDataTwo.map((specialty) => (
+            <li key={specialty.id}>
+              <h2>{specialty.name}</h2>
+            </li>
+          ))}
+        </ul>
+      </div>
     </main>
   );
 }
