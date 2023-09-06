8f22d3bd4551ace52799fb5b3e60f6f10539bdec
CeezR <forthomme.cesar@gmail.com>
2023-09-06 14:36:16 +0200

    feat: roadMap chat component with style

---
+++ front-end/app/components/RoadMapChat.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../roadMapChat.css'
+
+function RoadMapChat() {
+  return (
+    <div className='chat chat_show'>RoadMapChat</div>
+  )
+}
+
+export default RoadMapChat
