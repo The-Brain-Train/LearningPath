428e595ee6572b2900830181ac1fbf718a85de3e
Atufa Shabnam <atufa.shabnam@gmail.com>
2023-09-11 13:28:07 +0200

    feat: the repository was updated

--- back-end/src/main/java/com/braintrain/backend/roadMaps/RoadMapService.java
+++ back-end/src/main/java/com/braintrain/backend/roadMaps/RoadMapService.java
@@ -1,8 +1,22 @@
 package com.braintrain.backend.roadMaps;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 
 @Service
 public class RoadMapService {
 
+    @Autowired
+    RoadMapRepository repo;
+
+    public List<RoadMapMeta> getAll() {
+        return repo.findAll();
+    }
+
+    public RoadMapMeta createRoadMapMeta(RoadMapMeta roadMapMeta) {
+        return repo.save(roadMapMeta);
+    }
 }
