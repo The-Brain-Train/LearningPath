b73811ee4eb004b17e7500bacfb6f4125b8d2586
Edward Arvinius <edward.arvinius@gmail.com>
2023-09-11 13:57:45 +0200

    feat: created insertion of dummy data

--- back-end/src/main/java/com/braintrain/backend/DatabaseInitializer.java
+++ back-end/src/main/java/com/braintrain/backend/DatabaseInitializer.java
@@ -1,2 +1,33 @@
-package com.braintrain.backend;public class DatabaseInitializer {
+package com.braintrain.backend;
+
+import com.braintrain.backend.roadMaps.RoadMapMeta;
+import com.braintrain.backend.roadMaps.RoadMapRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import java.util.Map;
+
+@Configuration
+public class DatabaseInitializer {
+
+    private final MongoTemplate mongoTemplate;
+    private final RoadMapRepository roadmapRepository;
+
+    public DatabaseInitializer(MongoTemplate mongoTemplate, RoadMapRepository roadmapRepository) {
+        this.mongoTemplate = mongoTemplate;
+        this.roadmapRepository = roadmapRepository;
+    }
+
+    @Bean
+    CommandLineRunner initDatabase(RoadMapRepository repository) {
+        mongoTemplate.dropCollection(RoadMapMeta.class);
+        return args -> {
+            repository.save(new RoadMapMeta("JavaScript"));
+            repository.save(new RoadMapMeta("Java"));
+            repository.save(new RoadMapMeta("Python"));
+            repository.save(new RoadMapMeta("C++"));
+        };
+    }
 }
