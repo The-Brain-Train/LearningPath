1cbe5aa3a572a5fb699df374ab370e30a76fd3d0
CeezR <forthomme.cesar@gmail.com>
2023-09-12 17:53:57 +0200

    chore: deleted test scripts

--- back-end/src/main/java/com/braintrain/backend/PersonRepository.java
+++
@@ -1,6 +0,0 @@
-package com.braintrain.backend;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PersonRepository extends MongoRepository<Person, String> {
-}
