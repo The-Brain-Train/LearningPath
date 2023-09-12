70e22a74e154dd05b06b00a53c8b133471c29d46
Edward Arvinius <edward.arvinius@gmail.com>
2023-09-12 09:31:20 +0200

    feat: created skeleton test code

--- back-end/src/test/java/com/braintrain/backend/roadMaps/RoadMapServiceTest.java
+++ back-end/src/test/java/com/braintrain/backend/roadMaps/RoadMapServiceTest.java
@@ -1,4 +1,24 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.braintrain.backend.roadMaps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 class RoadMapServiceTest {
-  
+
+    @Autowired
+    private final RoadMapService roadMapService;
+
+    RoadMapServiceTest(RoadMapService roadMapService) {
+        this.roadMapService = roadMapService;
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void createRoadMapMeta() {
+    }
 }
