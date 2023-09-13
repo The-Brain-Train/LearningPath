aa0c5d7533d0f1aebd371d29ad1e2289a39e3328
Atufa Shabnam <atufa.shabnam@gmail.com>
2023-09-13 10:59:25 +0200

    feat: made he post mapping to return uri

--- back-end/src/main/java/com/braintrain/backend/roadMaps/RoadMapController.java
+++ back-end/src/main/java/com/braintrain/backend/roadMaps/RoadMapController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -26,9 +27,12 @@ public class RoadMapController {
     }
 
     @PostMapping
-    public ResponseEntity<RoadMapMeta> createRoadMap(@RequestBody RoadMapMeta roadMapMeta){
-        return ResponseEntity.ok(service.createRoadMapMeta(roadMapMeta));
+    public ResponseEntity<RoadMapMeta> createRoadMap(@RequestBody RoadMapDTO roadMapDTO){
+        RoadMapMeta roadMapMeta = service.createRoadMap(roadMapDTO);
+        URI uri = URI.create("/api/roadmaps" + roadMapMeta.getRoadMapReferenceId());
+        return ResponseEntity.created(uri).build();
     }
+
 
 
 }
