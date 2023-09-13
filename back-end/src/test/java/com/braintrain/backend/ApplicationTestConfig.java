00f8d079c226e5ef1e1f316afb858e6c268099df
Atufa Shabnam <atufa.shabnam@gmail.com>
2023-09-13 11:00:01 +0200

    feat: created an Configuration class

--- back-end/src/test/java/com/braintrain/backend/ApplicationTestConfig.java
+++ back-end/src/test/java/com/braintrain/backend/ApplicationTestConfig.java
@@ -1,2 +1,18 @@
-package com.braintrain.backend;public class ApplicationTestConfig {
+package com.braintrain.backend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.context.annotation.Bean;
+
+
+@Configuration
+public class ApplicationTestConfig {
+    @Bean
+    public RestTemplate template(){
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+        return restTemplate;
+    }
 }
+
