1ca4f4e6cc11db99a9ccdbc87dc0d253b65dfc13
Luke Williams <lukewilliams10101@gmail.com>
2023-09-13 09:58:45 +0200

    feat: getRoadMap implemented

--- front-end/app/httpRequests.ts
+++ front-end/app/httpRequests.ts
@@ -1,10 +1,19 @@
-import { Roadmap } from "./types";
+import { Roadmap, RoadmapMeta } from "./types";
 
 export const getRoadmaps = async () => {
-  const response = await fetch(`http://localhost:8080/roadmaps`);
+  const response = await fetch(`http://localhost:8080/api/roadmaps`);
   if (!response.ok) {
     throw new Error("Failed to fetch roadmaps");
   }
-  const data: Roadmap[] = await response.json();
+  const data: RoadmapMeta[] = await response.json();
+  return data;
+};
+
+export const getRoadmap = async (id: string) => {
+  const response = await fetch(`http://localhost:8080/api/roadmaps/${id}`);
+  if (!response.ok) {
+    throw new Error("Failed to fetch roadmaps");
+  }
+  const data: Roadmap = await response.json();
   return data;
 };
