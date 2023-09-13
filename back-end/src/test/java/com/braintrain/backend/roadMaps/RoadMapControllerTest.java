7bfa02e36cc7a9006f017de1229173f0c94b6b35
Atufa Shabnam <atufa.shabnam@gmail.com>
2023-09-13 11:01:23 +0200

    test: tests for controller

--- back-end/src/test/java/com/braintrain/backend/roadMaps/RoadMapControllerTest.java
+++ back-end/src/test/java/com/braintrain/backend/roadMaps/RoadMapControllerTest.java
@@ -1,4 +1,45 @@
+package com.braintrain.backend.roadMaps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("test")
 class RoadMapControllerTest {
-  
+    @Value("${server.port}")
+    private int port;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Test
+    void getRoadMaps() {
+        String uri = "http://localhost:%s/api/roadMaps".formatted(port);
+        ResponseEntity<List<RoadMapMeta>> exchange = restTemplate.exchange(uri, HttpMethod.GET, HttpEntity.EMPTY,
+                new ParameterizedTypeReference<List<RoadMapMeta>>() {});
+        assertThat(exchange.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(exchange.hasBody()).isTrue();
+    }
+
+    @Test
+    void testGetRoadMap() {
+    }
+
+    @Test
+    void createRoadMap() {
+    }
 }
