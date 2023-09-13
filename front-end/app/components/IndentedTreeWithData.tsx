9b41e41f9d1a20b88d320e04aeface0d41796523
Luke Williams <lukewilliams10101@gmail.com>
2023-09-13 09:58:16 +0200

    feat: identedTree that takes data implemented

---
+++ front-end/app/components/IndentedTreeWithData.tsx
@@ -0,0 +1,147 @@
+"use client";
+
+import React, { useEffect, useRef, useState } from "react";
+import * as d3 from "d3";
+import { getResponseFromOpenAI } from "../openAIChat";
+
+type IndentedTreeProps = {
+  roadMapdata: JSON | null;
+};
+
+const IndentedTreeWithData = ({ roadMapdata }: IndentedTreeProps) => {
+  const [data, setData] = useState(null);
+  const svgRef = useRef(null);
+
+  const handleSendMessage = async () => {
+    const chatHistory = [
+      {
+        role: "user",
+        content: `I will provide you with JSON format, Please use it to generate a hirecical roadmap for learning ${topic}.  Please only provided the data and nothing else as it will be parsed. { "name": "${topic}" "children": []`,
+      },
+    ];
+    console.log("Fetching data");
+    try {
+      const response = await getResponseFromOpenAI(chatHistory);
+      console.log(response);
+      const jsonData = await JSON.parse(response.choices[0].message.content);
+      setData(jsonData);
+      console.log(jsonData);
+    } catch (error) {
+      console.error("Error parsing JSON:", error);
+    }
+  };
+  useEffect(() => {
+    if (topic != null) {
+      handleSendMessage();
+    }
+  }, [topic]);
+
+  useEffect(() => {
+    if (data == null) return;
+    const format = d3.format(",");
+    const nodeSize = 21;
+    const root = d3.hierarchy(data).eachBefore(
+      (
+        (i) => (d) =>
+          (d.index = i++)
+      )(0)
+    );
+    const nodes = root.descendants();
+
+    // Dynamically calculate width and height based on screen dimensions
+    const screenWidth = window.innerWidth;
+    const screenHeight = window.innerHeight;
+    const width = screenWidth;
+    const height = (nodes.length + 1) * nodeSize;
+
+    const columns = [
+      {
+        label: "Size",
+        value: (d) => d.value,
+        format,
+        x: screenWidth - 25,
+      },
+    ];
+
+    const svg = d3
+      .select(svgRef.current)
+      .attr("width", width)
+      .attr("height", height)
+      .attr("viewBox", [-nodeSize / 2, (-nodeSize * 3) / 2, width, height])
+      .attr(
+        "style",
+        "max-width: 100%; height: auto; font: 14px sans-serif; overflow: visible;"
+      );
+
+    const link = svg
+      .append("g")
+      .attr("fill", "none")
+      .attr("stroke", "#999")
+      .selectAll()
+      .data(root.links())
+      .join("path")
+      .attr(
+        "d",
+        (d) => `
+        M${d.source.depth * nodeSize},${d.source.index * nodeSize}
+        V${d.target.index * nodeSize}
+        h${nodeSize}
+      `
+      );
+
+    const node = svg
+      .append("g")
+      .selectAll()
+      .data(nodes)
+      .join("g")
+      .attr("transform", (d) => `translate(0,${d.index * nodeSize})`);
+
+    node
+      .append("circle")
+      .attr("cx", (d) => d.depth * nodeSize)
+      .attr("r", 2.5)
+      .attr("fill", (d) => (d.children ? null : "#999"));
+
+    node
+      .append("text")
+      .attr("dy", "0.32em")
+      .attr("x", (d) => d.depth * nodeSize + 6)
+      .text((d) => d.data.name);
+
+    node.append("title").text((d) =>
+      d
+        .ancestors()
+        .reverse()
+        .map((d) => d.data.name)
+        .join("/")
+    );
+
+    for (const { label, value, format, x } of columns) {
+      svg
+        .append("text")
+        .attr("dy", "0.32em")
+        .attr("y", -nodeSize)
+        .attr("x", x)
+        .attr("text-anchor", "end")
+        .attr("font-weight", "bold")
+        .text(label);
+
+      node
+        .append("text")
+        .attr("dy", "0.32em")
+        .attr("x", x)
+        .attr("text-anchor", "end")
+        .attr("fill", (d) => (d.children ? null : "#555"))
+        .data(root.copy().sum(value).descendants())
+        .text((d) => format(d.value, d));
+    }
+  }, [data]);
+
+  return (
+    <>
+      <svg className="overflow-hidden" ref={svgRef}></svg>
+    </>
+  );
+};
+
+export default IndentedTreeWithData;
