f80f5f0a92a05bb11e682f875168e80355d87584
Atufa Shabnam <atufa.shabnam@gmail.com>
2023-09-14 09:53:49 +0200

    feat: filtering the roadmaps based on search query

--- front-end/app/explore/page.tsx
+++ front-end/app/explore/page.tsx
@@ -1,26 +1,43 @@
-"use client"
+"use client";
 
 import Paper from "@mui/material/Paper";
 import InputBase from "@mui/material/InputBase";
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { deleteRoadmap, getRoadmaps } from "../httpRequests";
 import { RoadmapMeta } from "../types";
-import DeleteIcon from '@mui/icons-material/Delete';
+import DeleteIcon from "@mui/icons-material/Delete";
 
 export default function Explore() {
-
   const [roadmaps, setRoadmaps] = useState<RoadmapMeta[]>([]);
+  const [filteredRoadmaps, setFilteredRoadmaps] = useState<RoadmapMeta[]>([]);
+  const [search, setSearch] = useState("");
 
-  useEffect(()=> {
-    getRoadmaps().then(data => setRoadmaps(data));
-  }, [])
+  useEffect(() => {
+    getRoadmaps().then((data) => setRoadmaps(data));
+  }, []);
 
-  const handleDelete = async (id:string) => {
+  useEffect(() => {
+    const filtered = roadmaps.filter((roadmap) =>
+      roadmap.name.toLowerCase().includes(search.toLowerCase())
+    );
+    setFilteredRoadmaps(filtered);
+  }, [search, roadmaps]);
+
+  const handleDelete = async (id: string) => {
     await deleteRoadmap(id);
-    setRoadmaps((prevRoadMaps) => prevRoadMaps.filter((roadmap) => roadmap.id !== id));
+    setRoadmaps((prevRoadmaps) =>
+      prevRoadmaps.filter((roadmap) => roadmap.id !== id)
+    );
+  };
+
+  const handleSearchChange = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const searchText = event.currentTarget.value;
+    setSearch(searchText);
   };
 
   return (
@@ -39,15 +56,20 @@ export default function Explore() {
           sx={{ ml: 1, flex: 1 }}
           placeholder="Search"
           inputProps={{ "aria-label": "search" }}
+          value={search}
+          onChange={handleSearchChange}
         />
         <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
           <SearchIcon />
         </IconButton>
       </Paper>
       <div className="roadMaps-list">
-          <ul className="flex flex-col justify-center ">
-            {[...roadmaps].reverse().map((roadMap: RoadmapMeta) => (
-              <li key={roadMap.id} className="bg-neutral mb-5 rounded-lg shadow-md">
+        <ul className="flex flex-col justify-center ">
+          {filteredRoadmaps.map((roadMap: RoadmapMeta) => (
+            <li
+              key={roadMap.id}
+              className="bg-neutral mb-5 rounded-lg shadow-md"
+            >
               <div className="roadmap-list-card flex justify-between items-center p-4">
                 <Link
                   className="card-list-text card-body text-left overflow-hidden"
@@ -57,27 +79,15 @@ export default function Explore() {
                     {roadMap.name}
                   </p>
                 </Link>
-            
+
                 <div className="flex-shrink-0 min-w-max">
                   <DeleteIcon onClick={() => handleDelete(roadMap.id)} />
                 </div>
               </div>
             </li>
-            
-            ))}
-          </ul>
-      </div>
-
-
-      {/* <div className="text-center">
-        <ul>
-          {roadmaps.map((roadMap, index) => (
-            <li className="border-2 rounded m-4 p-2" key={index}>
-              <Link key={roadMap.id} href={`/explore/${roadMap.roadMapReferenceId}`}>{roadMap.name}</Link>
-            </li>
           ))}
         </ul>
-      </div> */}
+      </div>
     </main>
   );
 }
