9720dba0efbcfd620b16115145a38a29c0fb3d5a
Atufa Shabnam <atufa.shabnam@gmail.com>
2023-09-14 14:46:54 +0200

    feat: added a header name

--- front-end/app/components/Header.tsx
+++ front-end/app/components/Header.tsx
@@ -1,19 +1,18 @@
 "use client";
+import Link from "next/link";
 import BurgerMenu from "./BurgerMenu";
 
 export default function Header() {
   return (
     <>
       <header className="bg-white fixed w-full h-16	">
-        <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
-          <div className="flex h-16 items-center justify-between">            
-              <a className="block text-teal-600" href="/">
-                <span className="sr-only">Home</span>
+          <div className="flex h-16 items-center justify-between mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">            
+              <Link href="/" className="flex text-xl font-semibold subpixel-antialiased font-serif	">
                 <img src="/learningpath-logo-cropped.png" alt="LP Logo" className="h-10" />
-              </a>
+                 Learning Path
+              </Link>
               <BurgerMenu />
           </div>
-        </div>
       </header>
     </>
   );
