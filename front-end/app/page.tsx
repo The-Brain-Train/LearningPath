648331eca201d1cbd8ed0e23b576db81e237149b
Luke Williams <lukewilliams10101@gmail.com>
2023-09-14 22:13:46 +0200

    style: made buttons on home page smaller

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -2,61 +2,60 @@
 import Header from "./components/Header";
 import Link from "next/link";
 import TextAnimation from "./components/TextAnimation";
-import './home.css'
+import "./home.css";
 
 export default function Home() {
-  
   return (
     <main className="home">
       <TextAnimation />
       <section className="flex flex-col p-3 gap-2 mt-5">
-      <div className="flex justify-center items-center">
-    <div className="info rounded p-5 relative text-white">
-
-          LearningPath is an AI-powered learning management tool. 
-          It designs personalized learning plans, estimates time required to reach skill levels, 
-          and offers a structured roadmap with attainable goals. These plans are tailored to users' academic levels, 
-          available study time, and pre-existing skills.
-        </div>
+        <div className="flex justify-center items-center">
+          <div className="info rounded p-5 relative text-white">
+            LearningPath is an AI-powered learning management tool. It designs
+            personalized learning plans, estimates time required to reach skill
+            levels, and offers a structured roadmap with attainable goals. These
+            plans are tailored to users' academic levels, available study time,
+            and pre-existing skills.
+          </div>
         </div>
 
         <div className="flex justify-center gap-2 mt-5">
-    <Link href="/explore">
-        <div
-            className="relative text-center p-4 rounded cursor-pointer group transition-opacity duration-300"
-            style={{ 
+          <Link href="/explore">
+            <div
+              className="relative text-center p-4 rounded cursor-pointer group transition-opacity duration-300"
+              style={{
                 backgroundImage: 'url("/roadmap2.jpeg")',
-                backgroundSize: 'cover',
-                backgroundPosition: 'center center',
-                width: '150px',
-                height: '100px',
-                opacity: 1
-            }}
-        >
-            <span className="absolute inset-0 flex items-center justify-center text-black font-bold group-hover:text-black">
+                backgroundSize: "cover",
+                backgroundPosition: "center center",
+                width: "120px",
+                height: "90px",
+                opacity: 1,
+              }}
+            >
+              <span className="absolute inset-0 flex items-center justify-center text-black font-bold group-hover:text-black">
                 Explore
-            </span>
-        </div>
-    </Link>
-    
-    <Link href="/create">
-        <div
-            className="relative text-center p-4 rounded cursor-pointer group transition-opacity duration-300"
-            style={{ 
+              </span>
+            </div>
+          </Link>
+
+          <Link href="/create">
+            <div
+              className="relative text-center p-4 rounded cursor-pointer group transition-opacity duration-300"
+              style={{
                 backgroundImage: 'url("/roadmap3.jpeg")',
-                backgroundSize: 'cover',
-                backgroundPosition: 'center center',
-                width: '150px',
-                height: '100px',
-                opacity: 1
-            }}
-        >
-            <span className="absolute inset-0 flex items-center justify-center text-black font-bold group-hover:text-black">
+                backgroundSize: "cover",
+                backgroundPosition: "center center",
+                width: "120px",
+                height: "90px",
+                opacity: 1,
+              }}
+            >
+              <span className="absolute inset-0 flex items-center justify-center text-black font-bold group-hover:text-black">
                 Create
-            </span>
+              </span>
+            </div>
+          </Link>
         </div>
-    </Link>
-</div>
       </section>
     </main>
   );
