be2bec713f0752b673ed3ff65cb649eee0d38107
Edward Arvinius <edward.arvinius@gmail.com>
2023-09-14 16:07:58 +0200

    style: fixed font and placement of Learning Path text in nav bar

--- front-end/app/components/Header.tsx
+++ front-end/app/components/Header.tsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import BurgerMenu from "./BurgerMenu";
 
+
 export default function Header() {
   return (
     <>
@@ -9,7 +10,9 @@ export default function Header() {
           <div className="flex h-16 items-center justify-between mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">            
               <Link href="/" className="flex text-xl font-semibold subpixel-antialiased font-serif	">
                 <img src="/learningpath-logo-cropped.png" alt="LP Logo" className="h-10" />
-                 Learning Path
+                <span style={{ fontFamily: 'Roboto', fontWeight: 900, marginTop: '5px', marginLeft: '10px' }}>Learning Path</span>
+
+                 
               </Link>
               <BurgerMenu />
           </div>
