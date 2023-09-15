28b173ee5c0e255c1b73c1f6203f7c38364c03f8
Luke Williams <lukewilliams10101@gmail.com>
2023-09-15 08:52:08 +0200

    fix: added type to roadMap useState

--- front-end/app/explore/[roadMapId]/page.tsx
+++ front-end/app/explore/[roadMapId]/page.tsx
@@ -1,10 +1,9 @@
-"use client"
+"use client";
 import IndentedTreeWithData from "@/app/components/IndentedTreeWithData";
 import { getRoadmap } from "@/app/httpRequests";
 import { useEffect, useState } from "react";
 import { ArrowBack } from "@mui/icons-material";
 import { useRouter } from "next/navigation";
-
 
 type Props = {
   params: {
@@ -13,20 +12,18 @@ type Props = {
 };
 
 export default function roadMapId(props: Props) {
-
-  const [roadMap, setRoadmap] = useState();
+  const [roadMap, setRoadmap] = useState<JSON | null>(null);
   const router = useRouter();
 
-  useEffect(()=> {
+  useEffect(() => {
     getRoadmap(props.params.roadMapId)
-    .then(data => JSON.parse(data.obj))
-    .then(data => setRoadmap(data));
-  }, [])
-
+      .then((data) => JSON.parse(data.obj))
+      .then((data) => setRoadmap(data));
+  }, []);
 
   return (
     <main>
-      <ArrowBack fontSize="medium" onClick={() => router.back()}/>
+      <ArrowBack fontSize="medium" onClick={() => router.back()} />
       <IndentedTreeWithData data={roadMap} />
     </main>
   );
