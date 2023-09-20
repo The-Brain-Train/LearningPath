00a3f5530fdb24423665912b42af31904a4eab06
Luke Williams <lukewilliams10101@gmail.com>
2023-09-20 11:39:25 +0200

    feat: added error for invalid topic

--- front-end/app/components/SearchTopic.tsx
+++ front-end/app/components/SearchTopic.tsx
@@ -17,8 +17,19 @@ export default function SearchTopic({
 }: roadMapChatProps) {
   const [userMessage, setUserMessage] = useState<string>("");
   const [messages, setMessages] = useState<Message[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  const containsOnlyNonAlphabet = (input: string) => {
+    return /^[^a-zA-Z]*$/.test(input);
+  };
 
   const handleSendMessage = async () => {
+    if (!userMessage.trim() || containsOnlyNonAlphabet(userMessage)) {
+      setError("Please enter a valid topic");
+      setTimeout(() => setError(null), 3000);
+      return;
+    }
+
     setTopic(userMessage);
     const chatHistory = [...messages, { role: "user", content: userMessage }];
     setMessages(chatHistory);
@@ -27,27 +38,30 @@ export default function SearchTopic({
   };
 
   return (
-    <div className="pt-4 mb-2  flex justify-center items-center ">
-      <div className="relative flex max-w-2xl" style={{ minWidth: "325px" }}>
-        <input
-          type="text"
-          value={userMessage}
-          onChange={(e) => setUserMessage(e.target.value)}
-          placeholder="Enter Topic!"
-          className="rounded-l-md	 w-full focus:outline-none focus:placeholder-gray-400 text-center text-gray-600 placeholder-gray-600 bg-gray-200  py-3"
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              handleSendMessage();
-            }
-          }}
-        />
-        <div className="rounded-r-md bg-gray-200">
-          <SendIcon
-            onClick={handleSendMessage}
-            sx={{ fontSize: 35, marginTop: "4px" }}
+    <>
+      <div className="pt-4 mb-2  flex justify-center items-center ">
+        <div className="relative flex max-w-2xl" style={{ minWidth: "325px" }}>
+          <input
+            type="text"
+            value={userMessage}
+            onChange={(e) => setUserMessage(e.target.value)}
+            placeholder="Enter Topic!"
+            className="rounded-l-md	 w-full focus:outline-none focus:placeholder-gray-400 text-center text-gray-600 placeholder-gray-600 bg-gray-200  py-3"
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                handleSendMessage();
+              }
+            }}
           />
+          <div className="rounded-r-md bg-gray-200">
+            <SendIcon
+              onClick={handleSendMessage}
+              sx={{ fontSize: 35, marginTop: "4px" }}
+            />
+          </div>
         </div>
       </div>
-    </div>
+      {error && <p className="text-red-500 font-bold">{error}</p>}
+    </>
   );
 }
