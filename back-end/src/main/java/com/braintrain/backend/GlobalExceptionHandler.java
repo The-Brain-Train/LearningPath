a262947b5efcba91d0c947916845629b3057ce1e
Luke Williams <lukewilliams10101@gmail.com>
2023-09-21 10:02:16 +0200

    feat: global exception handler implemented

--- back-end/src/main/java/com/braintrain/backend/GlobalExceptionHandler.java
+++ back-end/src/main/java/com/braintrain/backend/GlobalExceptionHandler.java
@@ -1,2 +1,22 @@
-package com.braintrain.backend;public class GlobalExceptionHandler {
+package com.braintrain.backend;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorMessageResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
+        ErrorMessageResponse errorResponse = new ErrorMessageResponse(
+                LocalDateTime.now(),
+                400,
+                "api/roadmaps",
+                ex.getMessage()
+        );
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
 }
