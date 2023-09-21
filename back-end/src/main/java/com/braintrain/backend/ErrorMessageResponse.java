dd50fb9d9c8878745611040e5a61833625a00b34
Atufa Shabnam <atufa.shabnam@gmail.com>
2023-09-21 10:38:09 +0200

    refactor: added lombok to the error response

--- back-end/src/main/java/com/braintrain/backend/ErrorMessageResponse.java
+++ back-end/src/main/java/com/braintrain/backend/ErrorMessageResponse.java
@@ -1,49 +1,14 @@
 package com.braintrain.backend;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import java.time.LocalDateTime;
 
+@Data
+@AllArgsConstructor
 public class ErrorMessageResponse {
     private LocalDateTime timestamp;
     private int status;
     private String path;
     private String message;
-
-    public ErrorMessageResponse(LocalDateTime timestamp, int status, String path, String message) {
-        this.timestamp = timestamp;
-        this.status = status;
-        this.path = path;
-        this.message = message;
-    }
-
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(LocalDateTime timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
