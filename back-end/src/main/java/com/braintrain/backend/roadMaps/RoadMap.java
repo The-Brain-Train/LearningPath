07391a270dc5bc1bba26e9690265a764db1901aa
Luke Williams <lukewilliams10101@gmail.com>
2023-09-21 09:27:04 +0200

    feat: implemented lombok to models

--- back-end/src/main/java/com/braintrain/backend/roadMaps/RoadMap.java
+++ back-end/src/main/java/com/braintrain/backend/roadMaps/RoadMap.java
@@ -1,45 +1,23 @@
 package com.braintrain.backend.roadMaps;
 
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Objects;
 
 @Document(collection = "roadmap")
+@Data
+@NoArgsConstructor
 public class RoadMap {
     @Id
     private String id;
 
     private String obj;
 
-    public RoadMap(){};
-
     public RoadMap(String obj) {
         this.obj = obj;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getObj() {
-        return obj;
-    }
-
-    public void setObj(String obj) {
-        this.obj = obj;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof RoadMap roadMap)) return false;
-        return Objects.equals(getId(), roadMap.getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId());
     }
 }
--- back-end/src/main/java/com/braintrain/backend/roadMaps/RoadMapMeta.java
+++ back-end/src/main/java/com/braintrain/backend/roadMaps/RoadMapMeta.java
@@ -1,11 +1,15 @@
 package com.braintrain.backend.roadMaps;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Objects;
 
 @Document(collection = "roadmap_meta")
+@Data
+@NoArgsConstructor
 public class RoadMapMeta {
     @Id
     private String id;
@@ -14,46 +18,8 @@ public class RoadMapMeta {
 
     private String roadMapReferenceId;
 
-    public RoadMapMeta(){};
-
-    public RoadMapMeta(String name) {
-        this.name = name;
-    }
-
     public RoadMapMeta(String name, String roadMapReferenceId) {
         this.name = name;
         this.roadMapReferenceId = roadMapReferenceId;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getRoadMapReferenceId() {
-        return roadMapReferenceId;
-    }
-
-    public void setRoadMapReferenceId(String roadMapReferenceId) {
-        this.roadMapReferenceId = roadMapReferenceId;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof RoadMapMeta that)) return false;
-        return Objects.equals(getId(), that.getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId());
     }
 }
