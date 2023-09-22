4b0944ed4d64a33042c0d72194a6078faac433a7
Atufa Shabnam <atufa.shabnam@gmail.com>
2023-09-22 10:37:31 +0200

    fix: fixed the prompt to give almost accurate value

--- front-end/app/functions/chatPreHistory.ts
+++ front-end/app/functions/chatPreHistory.ts
@@ -2,7 +2,7 @@ export const chatHistory = (topic: string | null, experienceLevel: string | null
   return [
     {
       role: "user",
-      content: `Generate a hierarchical roadmap for learning ${topic} with expected learning time for an average person using the provided JSON format. {"name": "${topic}", "children": [], "value": 10}. "value" should only be present in the last children`
+      content: `Generate a hierarchical roadmap for learning ${topic} with expected learning time for an person with ${experienceLevel} experience level using the provided JSON format. {"name": "${topic}", "children": [], "value": 10}. "value" should only be present in the last children. the total of value should be approx. ${hours}`
       },
   ];
 };
