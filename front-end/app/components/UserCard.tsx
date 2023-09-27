2439f13b9bcbfe580bd74cbe84a079edb5f402cf
Luke Williams <lukewilliams10101@gmail.com>
2023-09-27 16:30:01 +0200

    feat: personal road maps display with delete functionality

--- front-end/app/components/UserCard.tsx
+++ front-end/app/components/UserCard.tsx
@@ -1,49 +1,24 @@
-import Image from "next/image"
+import Image from "next/image";
+import { CardProps } from "../types";
 
-type User = {
-    name?: string | null | undefined;
-    email?: string | null | undefined;
-    image?: string | null | undefined;
-} | undefined
+export default function Card({ user }: CardProps) {
+  const userImage = user?.image ? (
+    <Image
+      className="border-4 border-black dark:border-slate-500 drop-shadow-xl shadow-black rounded-full mx-auto mt-8"
+      src={user?.image}
+      width={200}
+      height={200}
+      alt={user?.name ?? "Profile Pic"}
+      priority={true}
+    />
+  ) : null;
 
-type Props = {
-    user: User,
-    pagetype: string,
-}
-
-export default function Card({ user, pagetype }: Props) {
-
-    //console.log(user)
-
-    const greeting = user?.name ? (
-        <div className="flex flex-col items-center p-6 bg-white rounded-lg font-bold text-5xl text-black">
-            Hello {user?.name}!
-        </div>
-    ) : null
-
-    // const emailDisplay = user?.email ? (
-    //     <div className="flex flex-col items-center p-6 bg-white rounded-lg font-bold text-5xl text-black">
-    //         {user?.email}
-    //     </div>
-    // ) : null
-
-    const userImage = user?.image ? (
-        <Image
-            className="border-4 border-black dark:border-slate-500 drop-shadow-xl shadow-black rounded-full mx-auto mt-8"
-            src={user?.image}
-            width={200}
-            height={200}
-            alt={user?.name ?? "Profile Pic"}
-            priority={true}
-        />
-    ) : null
-
-    return (
-        <section className="flex flex-col gap-4 mt-20">
-            {greeting}
-            {/* {emailDisplay} */}
-            {userImage}
-            <p className="text-2xl text-center">{pagetype} Page!</p>
-        </section>
-    )
+  return (
+    <section className="flex flex-col gap-1 mt-20">
+      <div className="flex flex-col text-center items-center p-6 bg-white rounded-lg font-bold text-5xl text-black">
+        Hello {user?.name}!
+      </div>
+      {userImage}
+    </section>
+  );
 }
--- front-end/app/myprofile/page.tsx
+++ front-end/app/myprofile/page.tsx
@@ -1,17 +1,33 @@
-"use client"
+"use client";
 import React, { useEffect, useState } from "react";
 import UserCard from "../components/UserCard";
-import { getUsersRoadmapMetas } from "../functions/httpRequests";
+import { deleteRoadmap, getUsersRoadmapMetas } from "../functions/httpRequests";
 import { redirect } from "next/navigation";
 import { useSession } from "next-auth/react";
 import { RoadMapMetaList } from "../types";
+import Link from "next/link";
+import DeleteModal from "../components/DeleteModal";
 
 const page = () => {
   const { data: session, status } = useSession();
-  const [userRoadmaps, setUserRoadmaps] = useState<RoadMapMetaList | undefined>(undefined);
+  const [userRoadmaps, setUserRoadmaps] = useState<RoadMapMetaList | undefined>(
+    undefined
+  );
 
-  const handleSignInRedirect = () => {
-    redirect("api/auth/signin?callbackUrl=/home");
+  // const handleSignInRedirect = () => {
+  //   redirect("api/auth/signin?callbackUrl=/home");
+  // };
+
+  const handleDelete = async (id: string) => {
+    await deleteRoadmap(id);
+    setUserRoadmaps((prevRoadmaps) => {
+      if (!prevRoadmaps) return prevRoadmaps;
+      return {
+        roadMapMetaList: prevRoadmaps.roadMapMetaList?.filter(
+          (roadmap) => roadmap.id !== id
+        ),
+      };
+    });
   };
 
   useEffect(() => {
@@ -19,10 +35,8 @@ const page = () => {
       if (!session || !session.user || !session.user.email) {
         return;
       }
-
       const currentUser = session.user.email;
       const roadmapMetas = await getUsersRoadmapMetas(currentUser);
-      console.log(roadmapMetas);
       setUserRoadmaps(roadmapMetas);
     };
 
@@ -30,20 +44,40 @@ const page = () => {
   }, [session]);
 
   return (
-    <>
-      <UserCard user={session?.user} pagetype={"My Profile"} />
-      <h2>Your Roadmaps</h2>
-      {(!session || !session.user || !session.user.email) ? (
-        <div>
-          <h1 className="text-5xl">You Shall Not Pass!</h1>
-          <button onClick={handleSignInRedirect}>Log in</button>
-        </div>
-      ) : (
-        userRoadmaps?.roadMapMetaList.map((meta, index) => (
-          <p key={index}>{meta.name}</p>
-        ))
-      )}
-    </>
+    <main className="main-background min-h-max flex items-center flex-col">
+      <UserCard user={session?.user} />
+      <h2 className="pt-7 pl-2">My Roadmaps</h2>
+      <div
+        className="roadMaps-list"
+        style={{ maxWidth: "300px", width: "80%" }}
+      >
+        <ul className="flex flex-col justify-center ">
+          {userRoadmaps?.roadMapMetaList.map((meta, index) => (
+            <li
+              key={index}
+              className="bg-slate-300 mb-5 rounded-lg shadow-md"
+            >
+              <div className="roadmap-list-card flex justify-between items-center p-3">
+                <Link
+                  className="card-list-text card-body text-left overflow-hidden"
+                  href={`/explore/${meta.id}`}
+                >
+                  <p className="lyric-card-name overflow-ellipsis overflow-hidden whitespace-nowrap">
+                    {meta.name}
+                  </p>
+                </Link>
+                <div className="flex-shrink-0 min-w-max">
+                  <DeleteModal
+                    id={meta.id}
+                    onDelete={(id) => handleDelete(id)}
+                  />
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </main>
   );
 };
 
