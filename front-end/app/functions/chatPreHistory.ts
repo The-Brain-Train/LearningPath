792cf292158a3b6e088351f86f5c726eccc34095
Atufa Shabnam <atufa.shabnam@gmail.com>
2023-09-27 10:58:10 +0200

    fix: fixing the prompt

--- front-end/app/functions/chatPreHistory.ts
+++ front-end/app/functions/chatPreHistory.ts
@@ -2,6 +2,41 @@ export const chatHistory = (topic: string | null, experienceLevel: string | null
   return [
     {
       role: "user",
-      content: `Generate a hierarchical roadmap for learning ${topic} with expected learning time for an person with ${experienceLevel} experience level using the provided JSON format. {"name": "${topic}", "children": [], "value": 10}. "value" should only be present in the last children. the total of value should be approx. ${hours}`      },
+      content: `Generate a hierarchical roadmap for learning ${topic} for a person with ${experienceLevel} experience level using the provided JSON format.
+
+      The JSON structure should follow this format:
+      {
+        "name": "${topic}",
+        "children": [
+          {
+            "name": "Stage 1",
+            "children": [
+              {
+                "name": "Substage 1.1",
+                "children": [],
+                "value": 10
+              },
+              {
+                "name": "Substage 1.2",
+                "children": [],
+                "value": 15
+              }
+            ]
+          },
+          {
+            "name": "Stage 2",
+            "children": [
+              {
+                "name": "Substage 2.1",
+                "children": [],
+                "value": 20
+              }
+            ]
+          }
+        ],
+        "value": ${hours}
+      }
+      
+      ` },
   ];
 };
