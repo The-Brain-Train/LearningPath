ae5fa121ee57e31e2908e696ae8b2aede1132a52
Luke Williams <lukewilliams10101@gmail.com>
2023-09-28 10:53:13 +0200

    feat: implemented modal displaying favourites and personal roadmaps

--- front-end/app/myprofile/page.tsx
+++ front-end/app/myprofile/page.tsx
@@ -2,21 +2,45 @@
 import React, { useEffect, useState } from "react";
 import UserCard from "../components/UserCard";
 import { deleteRoadmap, getUsersRoadmapMetas } from "../functions/httpRequests";
-import { redirect } from "next/navigation";
 import { useSession } from "next-auth/react";
 import { RoadMapMetaList } from "../types";
 import Link from "next/link";
 import DeleteModal from "../components/DeleteModal";
+import {
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+} from "@material-tailwind/react";
+
+function Icon({ id, open }: { id: string | number; open: number }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={2}
+      stroke="currentColor"
+      className={`${
+        id === open ? "rotate-180" : ""
+      } h-5 w-5 transition-transform`}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+      />
+    </svg>
+  );
+}
 
 const page = () => {
-  const { data: session, status } = useSession();
+  const { data: session } = useSession();
   const [userRoadmaps, setUserRoadmaps] = useState<RoadMapMetaList | undefined>(
     undefined
   );
+  const [open, setOpen] = React.useState(0);
 
-  // const handleSignInRedirect = () => {
-  //   redirect("api/auth/signin?callbackUrl=/home");
-  // };
+  const handleOpen = (value: number) => setOpen(open === value ? 0 : value);
 
   const handleDelete = async (id: string) => {
     await deleteRoadmap(id);
@@ -44,38 +68,81 @@ const page = () => {
   }, [session]);
 
   return (
-    <main className="main-background min-h-max flex items-center flex-col">
-      <UserCard user={session?.user} />
-      <h2 className="pt-7 pl-2 text-white">My Roadmaps</h2>
-      <div
-        className="roadMaps-list"
-        style={{ maxWidth: "300px", width: "80%" }}
-      >
-        <ul className="flex flex-col justify-center ">
-          {userRoadmaps?.roadMapMetaList.map((meta, index) => (
-            <li
-              key={index}
-              className="bg-slate-300 mb-5 rounded-lg shadow-md"
-            >
-              <div className="roadmap-list-card flex justify-between items-center p-3">
-                <Link
-                  className="card-list-text card-body text-left overflow-hidden"
-                  href={`/explore/${meta.id}`}
+    <main className="main-background min-h-max ">
+      <div className="flex items-center flex-col pb-3">
+        <UserCard user={session?.user} />
+      </div>
+      <div className="mx-2">
+        <Accordion open={open === 1} icon={<Icon id={1} open={open} />}>
+          <AccordionHeader
+            onClick={() => handleOpen(1)}
+            className="bg-slate-300 p-3 dark:border-opacity-50"
+          >
+            <h2>My Roadmaps</h2>
+          </AccordionHeader>
+          <AccordionBody>
+            <ul className="flex flex-col justify-center">
+              {userRoadmaps?.roadMapMetaList.map((meta, index) => (
+                <li
+                  key={index}
+                  className="bg-slate-300 shadow-md w-full border-t-2 border-opacity-100 dark:border-opacity-50"
                 >
-                  <p className="lyric-card-name overflow-ellipsis overflow-hidden whitespace-nowrap">
-                    {meta.name}
-                  </p>
-                </Link>
-                <div className="flex-shrink-0 min-w-max">
-                  <DeleteModal
-                    id={meta.id}
-                    onDelete={(id) => handleDelete(id)}
-                  />
-                </div>
-              </div>
-            </li>
-          ))}
-        </ul>
+                  <div className="flex justify-between items-center p-2">
+                    <Link
+                      className="card-list-text card-body text-left overflow-hidden"
+                      href={`/explore/${meta.id}`}
+                    >
+                      <p className="lyric-card-name overflow-ellipsis overflow-hidden whitespace-nowrap pl-1">
+                        {meta.name}
+                      </p>
+                    </Link>
+                    <div className="flex-shrink-0 min-w-max">
+                      <DeleteModal
+                        id={meta.id}
+                        onDelete={(id) => handleDelete(id)}
+                      />
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </AccordionBody>
+        </Accordion>
+        <Accordion open={open === 2} icon={<Icon id={2} open={open} />}>
+          <AccordionHeader
+            onClick={() => handleOpen(2)}
+            className="bg-slate-300 p-3 border-t-2 border-black"
+          >
+            My Favourites
+          </AccordionHeader>
+          <AccordionBody>
+            <ul className="flex flex-col justify-center">
+              {userRoadmaps?.roadMapMetaList.map((meta, index) => (
+                <li
+                  key={index}
+                  className="bg-slate-300 shadow-md w-full border-t-2 border-opacity-100 dark:border-opacity-50"
+                >
+                  <div className="flex justify-between items-center p-2">
+                    <Link
+                      className="card-list-text card-body text-left overflow-hidden"
+                      href={`/explore/${meta.id}`}
+                    >
+                      <p className="lyric-card-name overflow-ellipsis overflow-hidden whitespace-nowrap pl-1">
+                        {meta.name}
+                      </p>
+                    </Link>
+                    <div className="flex-shrink-0 min-w-max">
+                      <DeleteModal
+                        id={meta.id}
+                        onDelete={(id) => handleDelete(id)}
+                      />
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </AccordionBody>
+        </Accordion>
       </div>
     </main>
   );
