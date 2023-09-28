3ec511af5d0b0016714908c1aa507d4029d520e7
Luke Williams <lukewilliams10101@gmail.com>
2023-09-28 14:07:33 +0200

    feat: hours + experience level added to personalRoadmapCard

--- front-end/app/components/PersonalRoadmapCard.tsx
+++ front-end/app/components/PersonalRoadmapCard.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { PersonalRoadmapCardProps } from "../types";
 import DeleteModal from "./DeleteModal";
+import { generateStarsforExperienceLevel } from "../functions/generateStarsForExperience";
 
 export default function PersonalRoadmapCard({
   roadmapMeta,
@@ -8,16 +9,25 @@ export default function PersonalRoadmapCard({
 }: PersonalRoadmapCardProps) {
   return (
     <li className="bg-slate-300 shadow-md w-full border-t-2 border-opacity-100 dark:border-opacity-50">
-      <div className="flex justify-between items-center p-2">
+      <div className="flex items-center p-2">
         <Link
-          className="text-left overflow-hidden"
+          className="text-left overflow-hidden flex flex-1 justify-center items-center"
           href={`/explore/${roadmapMeta.id}`}
         >
-          <p className="lyric-card-name overflow-ellipsis overflow-hidden whitespace-nowrap pl-1">
+          <p className="overflow-ellipsis overflow-hidden whitespace-nowrap pl-1 flex-1">
             {roadmapMeta.name}
           </p>
+          <div className="flex flex-col items-center justify-center">
+            <p className="overflow-ellipsis overflow-hidden whitespace-nowrap pl-1">
+              {generateStarsforExperienceLevel(roadmapMeta.experienceLevel)}
+            </p>
+            <p className="overflow-ellipsis overflow-hidden whitespace-nowrap pl-1">
+              {roadmapMeta.hours} hours
+            </p>
+          </div>
         </Link>
-        <div className="flex-shrink-0 min-w-max">
+        <div className="flex-shrink-0 min-w-max flex">
+          
           <DeleteModal
             id={roadmapMeta.id}
             onDelete={(id) => handleDelete(id)}
