1694534ee4be04c9ae7f2f638ee8cd2c575eb964
Atufa Shabnam <atufa.shabnam@gmail.com>
2023-10-02 14:19:01 +0200

    refactor removed unused imports

--- back-end/src/main/java/com/braintrain/backend/model/RoadMapMetaListDTO.java
+++ back-end/src/main/java/com/braintrain/backend/model/RoadMapMetaListDTO.java
@@ -1,7 +1,5 @@
 package com.braintrain.backend.model;
 
-import com.braintrain.backend.model.RoadMapMeta;
-
 import java.util.List;
 
 public record RoadMapMetaListDTO(List<RoadMapMeta> roadMapMetaList) {
