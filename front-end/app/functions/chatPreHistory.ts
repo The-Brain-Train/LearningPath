c8665df02681fec92e17a93b14dbe7f5296d46af
Vijani <vijanipiyawardana@gmail.com>
2023-10-05 11:02:27 +0200

    prompt partially done

--- front-end/app/functions/chatPreHistory.ts
+++ front-end/app/functions/chatPreHistory.ts
@@ -1,8 +1,9 @@
 export const chatHistory = (topic: string | null, experienceLevel: string | null, hours: number | null) => {
+  console.log("hours inside chatHistory: " + hours)
   return [
     {
       role: "user",
-      content: `Generate a hierarchical roadmap for learning ${topic} for a person with ${experienceLevel} experience level using the provided JSON format.
+      content: `Generate a hierarchical roadmap for learning ${topic} for a person with ${experienceLevel} experience level, by spending exactly ${hours} hours, using the provided JSON format.
 
       The JSON structure should follow this format:
       {
@@ -10,6 +11,7 @@ export const chatHistory = (topic: string | null, experienceLevel: string | null
         "children": [],
       }
       in the last set of children in the data there should be a '"value": provide hours for task' that represents the hours for that task. These combined should equal around ${hours} hours.
+      The total sum of "value": should be exactly equal to ${hours} hours, it should not exceed ${hours} hours or should not less than ${hours} hours.
       ` },
   ];
 };
