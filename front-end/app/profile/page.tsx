e91a67203ea2fc0e176dcab6f74791d556397823
Luke Williams <lukewilliams10101@gmail.com>
2023-10-10 16:06:17 +0200

    feat: can show signed in user details on profile

--- front-end/app/profile/page.tsx
+++ front-end/app/profile/page.tsx
@@ -2,15 +2,32 @@
 import React, { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 import jwtDecode from "jwt-decode";
+import { User } from "../types";
 
 const Profile = () => {
   const [cookies, setCookie] = useCookies(["user"]);
-  var user = jwtDecode(cookies.user);
+  var currentUser: User | null = null;
+
+  if (cookies.user) {
+    currentUser = jwtDecode(cookies.user);
+  }
+
+  useEffect(()=> {
+    console.log(currentUser);
+  }, [currentUser])
 
   return (
-    <>
-      <h1>My Page</h1>;
-    </>
+    <div className="mt-20">
+      <h1>My Page</h1>
+      {currentUser ? (
+        <>
+          <p>{currentUser.name}</p>
+          <p>{currentUser.email}</p>
+        </>
+      ) : (
+        <p>Loading user information...</p>
+      )}
+    </div>
   );
 };
 
