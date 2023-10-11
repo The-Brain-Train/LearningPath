85e93c0ead230d9ee733ce5c4d07a3cd80012672
Luke Williams <lukewilliams10101@gmail.com>
2023-10-11 15:57:43 +0200

    feat: postRoadmap now takes token

--- front-end/app/functions/httpRequests.ts
+++ front-end/app/functions/httpRequests.ts
@@ -44,11 +44,12 @@ export const deleteRoadmap = async (roadMapId: string) => {
   return roadMapId;
 };
 
-export const postRoadmap = async (roadmap: RoadmapDTO) => {
+export const postRoadmap = async (roadmap: RoadmapDTO, token: any) => {
   const response = await fetch(`http://localhost:8080/api/roadmaps`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      Authorization: "Bearer " + token,
     },
     body: JSON.stringify(roadmap),
   });
