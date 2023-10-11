09337798e6a39f62064b841884b0cf4c8a0a859f
Atufa Shabnam <atufa.shabnam@gmail.com>
2023-10-11 11:54:21 +0200

    style: added styling to the my profile page

--- front-end/app/components/UserCard.tsx
+++ front-end/app/components/UserCard.tsx
@@ -1,24 +1,17 @@
-import Image from "next/image";
 import { UserCardProps } from "../types";
+import AccountCircleSharpIcon from "@mui/icons-material/AccountCircleSharp";
 
 export default function Card({ user }: UserCardProps) {
-  const userImage = user?.image ? (
-    <Image
-      className="border-4 border-black dark:border-slate-500 drop-shadow-xl shadow-black rounded-full mx-auto "
-      src={user?.image}
-      width={150}
-      height={150}
-      alt={user?.name ?? "Profile Pic"}
-      priority={true}
-    />
-  ) : null;
-
   return (
     <section className="flex flex-col gap-1 ">
       <div className="flex flex-col text-center items-center p-6  rounded-lg font-bold text-2xl text-white">
         Hello {user?.name}!
       </div>
-      {userImage}
+      <div
+        className="border-4 dark:border-slate-500 drop-shadow-xl text-slate-300 rounded-full mx-auto "
+      >
+        <AccountCircleSharpIcon style={{ width: '150px', height: '150px' }}/>
+      </div>
     </section>
   );
 }
--- front-end/app/profile/page.tsx
+++ front-end/app/profile/page.tsx
@@ -4,7 +4,36 @@ import { useCookies } from "react-cookie";
 import jwtDecode from "jwt-decode";
 import { RoadmapMeta, User } from "../types";
 import { RoadmapMetaList } from "../types";
-import { getUserFavorites, getUsersRoadmapMetas } from "../functions/httpRequests";
+import { deleteRoadmap, getUserFavorites, getUsersRoadmapMetas, removeRoadmapMetaFromUserFavorites } from "../functions/httpRequests";
+import UserCard from "../components/UserCard";
+import {
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+} from "@material-tailwind/react";
+import PersonalRoadmapCard from "../components/PersonalRoadmapCard";
+import FavoriteRoadmapCard from "../components/FavoriteRoadmapCard";
+
+function Icon({ id, open }: { id: string | number; open: number }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={2}
+      stroke="currentColor"
+      className={`${
+        id === open ? "rotate-180" : ""
+      } h-5 w-5 transition-transform`}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+      />
+    </svg>
+  );
+}
 
 const Profile = () => {
   const [cookies, setCookie] = useCookies(["user"]);
@@ -13,7 +42,22 @@ const Profile = () => {
     undefined
   );
   const [favorites, setFavorites] = useState<RoadmapMeta[]>([]);
+  const [open, setOpen] = React.useState(0);
 
+  const handleOpen = (value: number) => setOpen(open === value ? 0 : value);
+
+  const handleDelete = async (roadmapMeta: RoadmapMeta) => {
+    await deleteRoadmap(roadmapMeta.id);
+    setUserRoadmaps((prevRoadmaps) => {
+      if (!prevRoadmaps) return prevRoadmaps;
+      return {
+        roadmapMetaList: prevRoadmaps.roadmapMetaList?.filter(
+          (roadmap) => roadmap.id !== roadmapMeta.id
+        ),
+      };
+    });
+  };
+  
   const fetchUserRoadmaps = async () => {
     try {
       if (currentUser?.email) { 
@@ -44,31 +88,80 @@ const Profile = () => {
   }, [cookies.user]);
 
   useEffect(() => {
-    fetchUserRoadmaps();
-    fetchUserFavorites();
+    if (currentUser) { 
+      fetchUserRoadmaps();
+      fetchUserFavorites();
+    }
   }, [currentUser, cookies.user]);
 
+  const handleRemoveFromFavorites = async (roadmapMeta: RoadmapMeta) => {
+    try {
+      await removeRoadmapMetaFromUserFavorites(
+       currentUser?.email,
+        roadmapMeta
+      );
+      setFavorites((prevFavorites) =>
+        prevFavorites.filter((favorite) => favorite.id !== roadmapMeta.id)
+      );
+    } catch (error) {
+      console.error("Error removing roadmap from favorites:", error);
+    }
+  };
+
   return (
-    <div className="mt-20">
-      <h1>My Page</h1>
-      <div>
-      {currentUser ? (
-        <>
-          <h2>Hi {currentUser.name}, you are signed in with the email {currentUser.email}</h2>
-          <h3>My Roadmaps</h3>
-          {userRoadmaps ? (
-            userRoadmaps.roadmapMetaList.map((roadmapMeta: RoadmapMeta, index) => (
-              <p key={index}>{roadmapMeta.name}</p>
-            ))
-          ): (
-            <p>Loading Roadmaps</p>
-          )}
-        </>
-      ) : (
-        <p>Loading user information...</p>
-      )}
-      </div>
-    </div>
+    <>
+     <main className="main-background min-h-max ">
+     <div className="flex items-center flex-col pb-3">
+     {currentUser && <UserCard user={currentUser} />}
+     </div>
+     <div className="mx-2">
+       <Accordion open={open === 1} icon={<Icon id={1} open={open} />}>
+         <AccordionHeader
+           onClick={() => handleOpen(1)}
+           className="p-3 dark:border-opacity-50 text-white"
+           style={{ backgroundColor: "#141832" }}
+         >
+           <h2>My Roadmaps</h2>
+         </AccordionHeader>
+         <AccordionBody>
+           <ul className="flex flex-col justify-center">
+             {userRoadmaps?.roadmapMetaList.map((roadmapMeta, index) => (
+               <PersonalRoadmapCard
+                 roadmapMeta={roadmapMeta}
+                 key={index}
+                 handleDelete={handleDelete}
+               />
+             ))}
+           </ul>
+         </AccordionBody>
+       </Accordion>
+       <Accordion open={open === 2} icon={<Icon id={2} open={open} />}>
+         <AccordionHeader
+           onClick={() => handleOpen(2)}
+           className="p-3 dark:border-opacity-50 text-white"
+           style={{ backgroundColor: "#141832" }}
+         >
+           My Favourites
+         </AccordionHeader>
+         <AccordionBody>
+         {favorites ? (
+      <ul className="flex flex-col justify-center">
+        {favorites.map((roadmapMeta, index) => (
+          <FavoriteRoadmapCard
+            removeFavorite={handleRemoveFromFavorites}
+            roadmapMeta={roadmapMeta}
+            key={index}
+          />
+        ))}
+      </ul>
+    ) : (
+      <p className="text-slate-300">Your favorites list is empty.</p>
+    )}
+         </AccordionBody>
+       </Accordion>
+     </div>
+   </main>
+   </>
   );
 };
 
