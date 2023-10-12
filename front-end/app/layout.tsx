abea1105e7cb85a784c657cb6c6dc867e66e6339
Atufa Shabnam <atufa.shabnam@gmail.com>
2023-10-12 20:03:24 +0200

    feat: started boilerplate code to implement react query

--- front-end/app/layout.tsx
+++ front-end/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Providers from "./provider";
 import Header from "./components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -22,7 +23,7 @@ export default async function RootLayout({
       <body className={inter.className}>
         
         <Header/>
-        {children}
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
