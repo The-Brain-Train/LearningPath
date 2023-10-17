85a62f7db8c8b23fa028e892a33ba72799f08c87
Luke Williams <lukewilliams10101@gmail.com>
2023-10-17 16:25:51 +0200

    refactor: updated configs

--- back-end/src/test/java/com/braintrain/backend/ApplicationTestConfig.java
+++ back-end/src/test/java/com/braintrain/backend/ApplicationTestConfig.java
@@ -1,9 +1,6 @@
 package com.braintrain.backend;
 
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.context.annotation.Bean;
 
