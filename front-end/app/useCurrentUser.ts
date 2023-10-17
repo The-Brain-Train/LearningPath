d9c4c07c745aaa80b50bd3f936c0eb6ca7768558
vijanipiyawardana <vijanipiyawardana@gmail.com>
2023-10-17 12:50:03 +0200

    feat: added getUserToken function

--- front-end/app/useCurrentUser.ts
+++ front-end/app/useCurrentUser.ts
@@ -3,12 +3,18 @@ import jwtDecode from 'jwt-decode';
 import { useCookies } from 'react-cookie';
 import { User } from "./types";
 
+export const getUserToken = () => {
+    const [cookies] = useCookies(['user']);
+    return cookies.user;
+}
+
 const useCurrentUser = () => {
   const [cookies] = useCookies(['user']);
 
   return useQuery<User | null>(
     ['currentUser'],
     async () => {
+        console.log("cu > " + cookies.user)
       if (cookies.user) {
         const user = jwtDecode(cookies.user) as User | null;
         return user;
