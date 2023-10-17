a733c3ad2f366e15073b7cc6c9f5c67d85448bd4
Luke Williams <lukewilliams10101@gmail.com>
2023-10-17 11:25:48 +0200

    fix: added correct database pathway + rest template config

--- back-end/src/test/java/com/braintrain/backend/ApplicationTestConfig.java
+++ back-end/src/test/java/com/braintrain/backend/ApplicationTestConfig.java
@@ -1,5 +1,7 @@
 package com.braintrain.backend;
 
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
@@ -9,10 +11,8 @@ import org.springframework.context.annotation.Bean;
 @Configuration
 public class ApplicationTestConfig {
     @Bean
-    public RestTemplate template(){
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
-        return restTemplate;
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 }
 
