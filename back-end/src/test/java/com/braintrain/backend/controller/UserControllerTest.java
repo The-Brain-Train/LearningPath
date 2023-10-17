882485c1a12212518389b96fa7b433ee17b3e27d
Luke Williams <lukewilliams10101@gmail.com>
2023-10-17 11:28:24 +0200

    refactor: user creation + sign in tests moved to authentication test file

--- back-end/src/test/java/com/braintrain/backend/controller/UserControllerTest.java
+++ back-end/src/test/java/com/braintrain/backend/controller/UserControllerTest.java
@@ -27,56 +27,6 @@ class UserControllerTest {
     private int port;
     @Autowired
     RestTemplate restTemplate;
-    private static final String BASE_URL = "http://localhost:%s/api/user";
+    private static final String BASE_URL = "http://localhost:%s/api/auth";
 
-    @Autowired
-    private UserService userService;
-
-    @AfterEach
-    void tearDown() {
-        String email = "edwardsemail@gmail.com";
-
-        User user = userService.getUserByEmail(email);
-        if (user != null) {
-            userService.deleteUser(user);
-        }
-    }
-
-    @Test
-    void shouldCreateUserIfNonExisting() {
-        String uri = BASE_URL.formatted(port);
-        User user = new User("Edward", "edwardsemail@gmail.com", new ArrayList<>());
-        ResponseEntity<User> exchange = restTemplate.exchange(uri, HttpMethod.POST, new HttpEntity<>(user), User.class);
-        assertThat(exchange.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-        assertThat(exchange.getHeaders().getLocation()).isNotNull();
-    }
-
-    @Test
-    void shouldGetUserIfUserExists() {
-        String uri = BASE_URL.formatted(port);
-        User user = new User("Edward", "edwardsemail@gmail.com", new ArrayList<>());
-        restTemplate.exchange(uri, HttpMethod.POST, new HttpEntity<>(user), User.class);
-
-        ResponseEntity<User> exchange = restTemplate.exchange(uri, HttpMethod.POST, new HttpEntity<>(user), User.class);
-        assertThat(exchange.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(exchange.getBody()).isNotNull();
-    }
-
-    @Test
-    void shouldGetFavoritesForExistingUser() {
-        User user = new User("Edward", "edwardsemail@gmail.com", new ArrayList<>());
-        RoadmapMeta roadmapMeta = new RoadmapMeta("Java", "1245", "anotherEmail@gmail.com", "Beginner", 50);
-
-        String uriForCreateUser = "http://localhost:%s/api/user".formatted(port);
-        restTemplate.exchange(uriForCreateUser, HttpMethod.POST, new HttpEntity<>(user), User.class);
-
-        String postUri = "http://localhost:%s/api/roadmaps/%s/favorites".formatted(port, user.getEmail());
-        restTemplate.exchange(postUri, HttpMethod.POST, new HttpEntity<>(roadmapMeta), UserFavoritesDTO.class);
-
-        String getUri = "http://localhost:%s/api/user/%s/favorites".formatted(port, user.getEmail());
-        ResponseEntity<UserFavoritesDTO> getExchange = restTemplate.exchange(getUri, HttpMethod.GET, HttpEntity.EMPTY, UserFavoritesDTO.class);
-
-        assertThat(getExchange.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(getExchange.getBody()).isNotNull();
-    }
 }
