a868ab990460f620075d24907284969272b0c577
Luke Williams <lukewilliams10101@gmail.com>
2023-10-17 16:27:52 +0200

    test: sign in tests implemented and pass

--- back-end/src/test/java/com/braintrain/backend/controller/AuthenticationControllerTest.java
+++ back-end/src/test/java/com/braintrain/backend/controller/AuthenticationControllerTest.java
@@ -2,6 +2,7 @@ package com.braintrain.backend.controller;
 
 import com.braintrain.backend.model.User;
 import com.braintrain.backend.security.dao.JwtAuthenticationResponse;
+import com.braintrain.backend.security.dao.SignInRequest;
 import com.braintrain.backend.security.dao.SignUpRequest;
 import com.braintrain.backend.service.UserService;
 import org.junit.jupiter.api.AfterEach;
@@ -14,10 +15,11 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ActiveProfiles("test")
@@ -45,39 +47,59 @@ class AuthenticationControllerTest {
     void shouldCreateUserWhenUserSignsUp() {
         String uri = "http://localhost:%s/api/auth/signup".formatted(port);
         SignUpRequest signUpRequest = new SignUpRequest("Edward", "edwardsemail@gmail.com", "123", "USER");
+
         ResponseEntity<JwtAuthenticationResponse> exchange = restTemplate.exchange(uri, HttpMethod.POST, new HttpEntity<>(signUpRequest), JwtAuthenticationResponse.class);
+
         assertThat(exchange.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(exchange.getBody()).isNotNull();
         assertThat(exchange.getBody().getToken()).isNotNull();
     }
 
     @Test
     void creatingUserWithExistingEmailShouldThrow409() {
-        String uri = BASE_URL.formatted(port);
-        User user = new User("Edward", "edwardsemail@gmail.com");
-        ResponseEntity<User> exchange = restTemplate.exchange(uri, HttpMethod.POST, new HttpEntity<>(user), User.class);
-        assertThat(exchange.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-        assertThat(exchange.getHeaders().getLocation()).isNotNull();
+        String uri = "http://localhost:%s/api/auth/signup".formatted(port);
+        SignUpRequest signUpRequest = new SignUpRequest("Edward", "edwardsemail@gmail.com", "123", "USER");
+
+        restTemplate.exchange(uri, HttpMethod.POST, new HttpEntity<>(signUpRequest), JwtAuthenticationResponse.class);
+
+        try {
+            restTemplate.exchange(uri, HttpMethod.POST, new HttpEntity<>(signUpRequest), JwtAuthenticationResponse.class);
+            fail("Expected EmailAlreadyExistsException with status code 409");
+        } catch (HttpClientErrorException e) {
+            assertEquals(HttpStatus.CONFLICT, e.getStatusCode());
+        }
     }
 
     @Test
     void shouldGetUserWhenUserSignsIn() {
-        String uri = BASE_URL.formatted(port);
-        User user = new User("Edward", "edwardsemail@gmail.com");
-        restTemplate.exchange(uri, HttpMethod.POST, new HttpEntity<>(user), User.class);
+        String signUpURI = "http://localhost:%s/api/auth/signup".formatted(port);
+        String signInURI = "http://localhost:%s/api/auth/signin".formatted(port);
 
-        ResponseEntity<User> exchange = restTemplate.exchange(uri, HttpMethod.POST, new HttpEntity<>(user), User.class);
+        SignUpRequest signUpRequest = new SignUpRequest("Edward", "edwardsemail@gmail.com", "123", "USER");
+        SignInRequest signInRequest = new SignInRequest("edwardsemail@gmail.com", "123");
+
+        restTemplate.exchange(signUpURI, HttpMethod.POST, new HttpEntity<>(signUpRequest), JwtAuthenticationResponse.class);
+        ResponseEntity<JwtAuthenticationResponse> exchange = restTemplate.exchange(signInURI, HttpMethod.POST, new HttpEntity<>(signInRequest), JwtAuthenticationResponse.class);
+
         assertThat(exchange.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(exchange.getBody()).isNotNull();
+        assertThat(exchange.getBody().getToken()).isNotNull();
     }
 
     @Test
     void signingInWithIncorrectCredentialsShouldThrow403() {
-        String uri = BASE_URL.formatted(port);
-        User user = new User("Edward", "edwardsemail@gmail.com");
-        ResponseEntity<User> exchange = restTemplate.exchange(uri, HttpMethod.POST, new HttpEntity<>(user), User.class);
-        assertThat(exchange.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-        assertThat(exchange.getHeaders().getLocation()).isNotNull();
+        String signUpURI = "http://localhost:%s/api/auth/signup".formatted(port);
+        String signInURI = "http://localhost:%s/api/auth/signin".formatted(port);
+
+        SignUpRequest signUpRequest = new SignUpRequest("Edward", "edwardsemail@gmail.com", "123", "USER");
+        SignInRequest signInRequest = new SignInRequest("edwardsemai@gmail.com", "123");
+
+        restTemplate.exchange(signUpURI, HttpMethod.POST, new HttpEntity<>(signUpRequest), JwtAuthenticationResponse.class);
+
+        try {
+            restTemplate.exchange(signInURI, HttpMethod.POST, new HttpEntity<>(signInRequest), JwtAuthenticationResponse.class);
+            fail("Expected EmailAlreadyExistsException with status code 409");
+        } catch (HttpClientErrorException e) {
+            assertEquals(HttpStatus.FORBIDDEN, e.getStatusCode());
+        }
     }
 
 }
