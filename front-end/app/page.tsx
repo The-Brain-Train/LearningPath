60924df0a310a9224f23339dd97f02cb932280ba
Luke Williams <lukewilliams10101@gmail.com>
2023-10-20 11:17:33 +0200

    style: styling finished for home page

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -9,25 +9,25 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="main-background text-white">
-      <section className="flex flex-col p-3 gap-2 mt-5 sm:content-center sm:items-center">
+      <section className="flex flex-col p-3 gap-2 mt-5 sm:items-center">
         <h1 className="text-center pb-5 text-4xl font-extrabold sm:text-5xl sm:mb-5">
           Welcome to LearningPath
         </h1>
-        <div className="flex flex-col items-center max-w-3xl sm:flex-row-reverse sm:item sm:gap-3">
-        <Image
+        <div className="flex flex-col items-center max-w-4xl sm:flex-row-reverse sm:item sm:gap-3">
+          <Image
             src="/roadmap1.jpeg"
             alt="Create Roadmap"
-            className="mt-0 pb-5"
+            className="mt-0 pb-5 lg:w-80 basis-11/12 rounded-lg"
             height={280}
             width={280}
           />
           <div>
-            <p className="text-center pb-5">
+            <p className="text-center pb-5 sm:text-left">
               LearningPath is an exciting and innovative AI-powered learning
               management tool that&apos;s designed to make your learning journey
               both enjoyable and productive.
             </p>
-            <div className="flex justify-center gap-4 mt-3">
+            <div className="flex justify-center gap-4 mt-3 sm:justify-start">
               <Link
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                 href="/explore"
@@ -42,9 +42,8 @@ export default function Home() {
               </Link>
             </div>
           </div>
-          
         </div>
-        <div className="flex flex-col pt-8 max-w-3xl text-center">
+        <div className="flex flex-col pt-8 max-w-4xl text-center">
           <h2 className="text-3xl mb-8 text-center font-bold">
             Features of LearningPath
           </h2>
@@ -56,16 +55,24 @@ export default function Home() {
                   Create Your Roadmap
                 </h2>
               </div>
-              <div className="flex flex-col items-center sm:flex-row sm:item sm:gap-3">
-                <p>
-                  Craft personalized learning roadmaps tailored to your goals
-                  and aspirations. Whether you want to master a new programming
-                  language or learn how to cook.
-                </p>
+              <div className="flex flex-col items-center sm:flex-row-reverse sm:item sm:gap-3 sm:text-left">
+                <div className="mb-6">
+                  <p className="mb-6 sm:mb-14">
+                    Craft personalized learning roadmaps tailored to your goals
+                    and aspirations. Whether you want to master a new
+                    programming language or learn how to cook.
+                  </p>
+                  <Link
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    href="/create"
+                  >
+                    Create Roadmap
+                  </Link>
+                </div>
                 <Image
                   src="/roadmap3.jpeg"
                   alt="Create Roadmap"
-                  className="mt-2 pb-3"
+                  className="mt-2 pb-3 basis-9/12 rounded-lg"
                   height={250}
                   width={250}
                 />
@@ -78,15 +85,24 @@ export default function Home() {
                   Save &amp; Manage Roadmaps
                 </h2>
               </div>
-              <div className="flex flex-col items-center sm:flex-row sm:item sm:gap-3">
-                <p>
-                  Organize your learning journey by saving your roadmaps. Easily
-                  access and update them as you progress toward your goals.
-                </p>
+              <div className="flex flex-col items-center sm:flex-row sm:gap-3 sm:text-left">
+                <div className="mb-6">
+                  <p className="mb-6 sm:mb-14">
+                    Organize your learning journey by creating an account and saving your roadmaps.
+                    Easily access roadmaps you have created and progress towards your
+                    goals.
+                  </p>
+                  <Link
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    href="/signup"
+                  >
+                    Sign Up
+                  </Link>
+                </div>
                 <Image
                   src="/roadmap4.jpg"
                   alt="Create Roadmap"
-                  className="mt-2 pb-3"
+                  className="mt-2 pb-3 basis-9/12 rounded-lg"
                   height={250}
                   width={250}
                 />
@@ -96,20 +112,28 @@ export default function Home() {
               <div className="flex items-center mb-2">
                 <h2 className="text-white text-2xl font-bold mb-2">
                   <ThumbsUpDownIcon className="mr-2" />
-                  Explore &amp; Vote
+                  Explore Community Roadmaps
                 </h2>
               </div>
-              <div className="flex flex-col items-center sm:flex-row sm:item sm:gap-3">
-                <p>
-                  Dive into a world of learning possibilities by exploring
-                  roadmaps created by fellow learners. Upvote the ones you find
-                  most valuable, helping others discover the best learning paths
-                  for specific skills.
-                </p>
+              <div className="flex flex-col items-center sm:flex-row-reverse sm:item sm:gap-3 sm:text-left">
+                <div className="mb-6">
+                  <p className="mb-6 sm:mb-14">
+                    Dive into a world of learning possibilities by exploring
+                    roadmaps created by fellow learners. Upvote the ones you
+                    find most valuable, helping others discover the best
+                    learning paths for specific skills.
+                  </p>
+                  <Link
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    href="/explore"
+                  >
+                    Explore
+                  </Link>
+                </div>
                 <Image
                   src="/roadmap2.jpeg"
                   alt="Create Roadmap"
-                  className="mt-2 pb-3"
+                  className="mt-2 pb-3 basis-10/12 rounded-lg"
                   height={250}
                   width={250}
                 />
