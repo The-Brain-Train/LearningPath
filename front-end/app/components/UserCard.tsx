14b49f92edf50ceb143e1a15888c31a8a6455ef2
Luke Williams <lukewilliams10101@gmail.com>
2023-10-23 11:49:57 +0200

    feat: began implementing user profile picture change

--- front-end/app/components/UserCard.tsx
+++ front-end/app/components/UserCard.tsx
@@ -1,4 +1,5 @@
 import { UserCardProps } from "../util/types";
+import Image from "next/image";
 import AccountCircleSharpIcon from "@mui/icons-material/AccountCircleSharp";
 
 export default function Card({ user }: UserCardProps) {
@@ -7,10 +8,9 @@ export default function Card({ user }: UserCardProps) {
       <div className="flex flex-col text-center items-center p-6  rounded-lg font-bold text-2xl text-white">
         Hello {user?.name}!
       </div>
-      <div
-        className="border-4 dark:border-slate-500 drop-shadow-xl text-slate-300 rounded-full mx-auto "
-      >
-        <AccountCircleSharpIcon className="sm:h-32" style={{ width: '220px', height: '220px' }}/>
+      <div className="border-4 dark:border-slate-500 drop-shadow-xl text-slate-300 rounded-full mx-auto ">
+        <Image src="./icon-profile.png" width={220} height={220} alt="User profile picture"/>
+        {/* <AccountCircleSharpIcon className="sm:h-32" style={{ width: '220px', height: '220px' }}/> */}
       </div>
     </section>
   );
