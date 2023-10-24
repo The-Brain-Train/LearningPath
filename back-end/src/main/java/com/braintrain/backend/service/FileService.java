9c91e73e3bb864ffced1a77d6b7301abe06f39f2
Luke Williams <lukewilliams10101@gmail.com>
2023-10-24 14:21:46 +0200

    feat: storing file to google cloud service logic

--- back-end/src/main/java/com/braintrain/backend/service/FileService.java
+++ back-end/src/main/java/com/braintrain/backend/service/FileService.java
@@ -1,2 +1,79 @@
-package com.braintrain.backend.service;public class FileService {
+package com.braintrain.backend.service;
+
+import com.braintrain.backend.controller.dtos.FileDTO;
+import com.braintrain.backend.exceptionHandler.exception.GCPFileUploadException;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.Bucket;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.util.UUID;
+
+@Component
+public class FileService {
+    @Value("${gcp.config.file}")
+    private String gcpConfigFile;
+
+    @Value("${gcp.project.id}")
+    private String gcpProjectId;
+
+    @Value("${gcp.bucket.id}")
+    private String gcpBucketId;
+
+    @Value("${gcp.dir.name}")
+    private String gcpDirectoryName;
+
+    public FileDTO uploadFile(MultipartFile multipartFile, String fileName, String contentType) throws IOException {
+        try {
+            byte[] fileData = FileUtils.readFileToByteArray(convertFile(multipartFile));
+
+            InputStream inputStream = new ClassPathResource(gcpConfigFile).getInputStream();
+
+            StorageOptions options = StorageOptions.newBuilder().setProjectId(gcpProjectId)
+                    .setCredentials(GoogleCredentials.fromStream(inputStream)).build();
+
+            Storage storage = options.getService();
+            Bucket bucket = storage.get(gcpBucketId, Storage.BucketGetOption.fields());
+
+            UUID uuid = UUID.randomUUID();
+            String id = uuid.toString();
+
+            Blob blob = bucket.create(gcpDirectoryName + "/" + fileName + "-" + id + checkFileExtension(fileName), fileData, contentType);
+
+            if (blob != null) {
+                return new FileDTO(blob.getName(), blob.getMediaLink());
+            }
+        }catch (Exception e){
+            throw new GCPFileUploadException("An error occurred while storing data to GCS");
+        }
+        throw new GCPFileUploadException("An error occurred while storing data to GCS");
+    }
+
+    private File convertFile(MultipartFile file) throws IOException {
+        File convertedFile = new File(file.getOriginalFilename());
+        FileOutputStream outputStream = new FileOutputStream(convertedFile);
+        outputStream.write(file.getBytes());
+        outputStream.close();
+        return convertedFile;
+    }
+
+    private String checkFileExtension(String fileName) {
+        if(fileName != null && fileName.contains(".")){
+            String[] extensionList = {".png", ".jpeg", ".pdf", ".doc", ".mp3"};
+            for(String extension: extensionList) {
+                if (fileName.endsWith(extension)) {
+                    return extension;
+                }
+            }
+        }
+        return null;
+    }
 }
