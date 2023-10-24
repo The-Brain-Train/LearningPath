6acf2c3ff553427658a2370e60a8d8761d5b5b57
Luke Williams <lukewilliams10101@gmail.com>
2023-10-24 14:18:57 +0200

    feat: implemeneted update and get profile picture

--- back-end/src/main/java/com/braintrain/backend/controller/UserController.java
+++ back-end/src/main/java/com/braintrain/backend/controller/UserController.java
@@ -1,14 +1,16 @@
 package com.braintrain.backend.controller;
 
 import com.braintrain.backend.model.User;
-import com.braintrain.backend.controller.dtos.UserFavoritesDTO;
-import com.braintrain.backend.controller.dtos.UserListDTO;
 import com.braintrain.backend.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.net.URI;
+
+import java.io.IOException;
+
 
 @RestController
 @RequestMapping("api/users")
@@ -17,5 +19,17 @@ import java.net.URI;
 public class UserController {
 
     private final UserService userService;
+    private final ResourceLoader resourceLoader;
+
+    @PostMapping("/{userEmail}/profileImage")
+    public ResponseEntity<String> updateProfilePicture(@PathVariable String userEmail, @RequestParam("file") MultipartFile file) throws IOException {
+        return ResponseEntity.ok(userService.uploadFile(userEmail, file));
+    }
+
+    @GetMapping("/{userEmail}/profileImage")
+    public ResponseEntity<String> getProfilePicture(@PathVariable String userEmail) {
+        User user = userService.getUserByEmail(userEmail);
+        return ResponseEntity.ok(user.getProfilePicture());
+    }
 
 }
