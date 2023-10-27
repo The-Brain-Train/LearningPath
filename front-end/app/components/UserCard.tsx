b13151512ce908a0a185247fc6763c7049fbd721
vijanipiyawardana <vijanipiyawardana@gmail.com>
2023-10-27 16:43:44 +0200

    fix: centered elements of profile page - user card

--- front-end/app/components/UserCard.tsx
+++ front-end/app/components/UserCard.tsx
@@ -50,7 +50,7 @@ export default function Card({ user }: UserCardProps) {
   );
 
   return (
-    <section className="flex flex-col gap-1 ">
+    <section className="flex flex-col gap-1 justify-center items-center">
       <div className="flex flex-col text-center items-center p-6  rounded-lg font-bold text-2xl text-white">
         Hello {user?.name}!
       </div>
@@ -73,7 +73,7 @@ export default function Card({ user }: UserCardProps) {
           />
         )}
       </div>
-      <div className="ml-14">
+      <div>
         <label className="text-white underline cursor-pointer text-sm">
           Change Profile Picture
           <input
