0d38123fc9f7d49b0d1ec137875a918ca5ae2607
vijanipiyawardana <vijanipiyawardana@gmail.com>
2023-11-06 16:29:19 +0100

    feat: added Converter class for RoadmapMeta, for pagination

---
+++ back-end/src/main/java/com/braintrain/backend/util/RoadmapMetaConverter.java
@@ -0,0 +1,30 @@
+package com.braintrain.backend.util;
+
+import com.braintrain.backend.controller.dtos.RoadmapMetaDTO;
+import com.braintrain.backend.model.RoadmapMeta;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class RoadmapMetaConverter {
+
+    public static Page<RoadmapMetaDTO> toRoadmapMetaDtoList(Page<RoadmapMeta> roadmapMetas) {
+        List<RoadmapMetaDTO> dtos = roadmapMetas.stream()
+                .map(RoadmapMetaConverter::toRoadmapMetaDto)
+                .toList();
+        return new PageImpl<>(dtos, roadmapMetas.getPageable(), roadmapMetas.getTotalElements());
+
+    }
+    public static RoadmapMetaDTO toRoadmapMetaDto(RoadmapMeta roadmapMeta) {
+        return new RoadmapMetaDTO(
+                roadmapMeta.getName(),
+                roadmapMeta.getRoadmapReferenceId(),
+                roadmapMeta.getUserEmail(),
+                roadmapMeta.getExperienceLevel(),
+                roadmapMeta.getHours()
+        );
+    }
+}
