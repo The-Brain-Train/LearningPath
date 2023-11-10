c5e8120952a9704ff52eb6dbdacf4f20581f04a9
vijanipiyawardana <vijanipiyawardana@gmail.com>
2023-11-10 16:52:38 +0100

    feat: customized method to filtered and paged roadmap retrieve

--- back-end/src/main/java/com/braintrain/backend/repository/RoadmapMetaRepository.java
+++ back-end/src/main/java/com/braintrain/backend/repository/RoadmapMetaRepository.java
@@ -1,8 +1,22 @@
 package com.braintrain.backend.repository;
 
 import com.braintrain.backend.model.RoadmapMeta;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface RoadmapMetaRepository extends MongoRepository<RoadmapMeta, String> {
+
+    Page<RoadmapMeta> findAllByNameContainingIgnoreCaseAndExperienceLevelContainingAndHoursBetween(
+            String name, String experienceLevel, int fromHour, int toHour, Pageable pageable);
+
+    default Page<RoadmapMeta> findAllFilteredPaged(
+            String name, String experienceLevel, int fromHour, int toHour, Pageable pageable) {
+        return findAllByNameContainingIgnoreCaseAndExperienceLevelContainingAndHoursBetween(
+                name, experienceLevel, fromHour, toHour, pageable);
+
+    }
+
+
 }
 
