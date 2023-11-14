a7069d72df848ef3844530275e95f9920ff5f611
vijanipiyawardana <vijanipiyawardana@gmail.com>
2023-11-14 14:48:36 +0100

    feat: added new prompts for chatgpt for the new fffeature - add resources

--- front-end/app/functions/chatPreHistory.ts
+++ front-end/app/functions/chatPreHistory.ts
@@ -13,3 +13,46 @@ export const chatHistory = (topic: string | null, experienceLevel: string | null
       ` },
   ];
 };
+
+export const requestPromptWithResources = (
+  topic: string | null, 
+  experienceLevel: string | null, 
+  hours: number | null,
+  resources: boolean) => {
+
+    return [
+      {
+        role: "user",
+  
+        content: `Generate a hierarchical roadmap for learning ${topic} for a person with ${experienceLevel} experience level, by spending exactly ${hours} hours, using the provided JSON format. 
+        
+        The JSON structure should follow this format:
+
+        {
+          "name": "${topic}",
+          "children": [],
+          "resources": [],
+        }
+        in the last set of children in the data there should be a '"value": provide hours for task' that represents the hours for that task. These combined should equal around ${hours} hours.
+        Additionally, include a "resources" array with details of 5 recommended resources (books, courses, websites) for learning Java, each with a name, type, and with actual link to the resource.
+        `,
+      },
+    ];
+
+};
+
+export const requestPromptOnlyResources = (
+  topic: string | null,
+  resources: boolean) => {
+    return [
+      {
+        role: "user",
+
+        content: `Provide a "resources" array with details of 5 recommended resources (books, courses, websites) for learning ${topic}, each with a name, type, and with actual link to the resource. 
+        Use the following JSON format:
+        {
+          “Resources”: [],
+        }`,
+      },
+    ];
+};
