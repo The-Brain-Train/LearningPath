3173da6b26e8dc08b4f7b0a89f940de28324ef3f
vijanipiyawardana <vijanipiyawardana@gmail.com>
2023-11-15 22:51:28 +0100

    test: added a method to create roadmaps from given data

--- back-end/src/test/java/com/braintrain/backend/TestHelper.java
+++ back-end/src/test/java/com/braintrain/backend/TestHelper.java
@@ -1,18 +1,31 @@
 package com.braintrain.backend;
 
 import com.braintrain.backend.controller.dtos.RoadmapDTO;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
 
 public class TestHelper {
     public static RoadmapDTO createRoadmapDTO(String name, Path path) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode jsonNode = objectMapper.readTree(new File(String.valueOf(path)));
         String str = objectMapper.writeValueAsString(jsonNode);
-        return new RoadmapDTO(name, str, "123@gmail.com", "Beginner", 10);
+        return new RoadmapDTO(name, str, "123@gmail.com", "beginner", 10);
     }
+
+    public static List<RoadmapDTO> createRoadmapDTOs(Path path) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        TypeReference<List<HashMap<String,String>>> typeRef = new TypeReference<>() {};
+        List<HashMap<String,String>> records = objectMapper.readValue(path.toFile(), typeRef);
+
+        return records.stream().map( m -> new RoadmapDTO(m.get("name"), m.get("obj"),
+                m.get("userEmail"), m.get("experienceLevel"), Integer.parseInt(m.get("hours")))).toList();
+    }
+
 }
