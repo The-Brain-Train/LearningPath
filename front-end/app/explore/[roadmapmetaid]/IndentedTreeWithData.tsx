73535995d87515e3414352b48a9f851ac5d95f71
Luke Williams <lukewilliams10101@gmail.com>
2023-11-21 16:33:32 +0100

    feat: began trying to implement check boxes

--- front-end/app/explore/[roadmapmetaid]/IndentedTreeWithData.tsx
+++ front-end/app/explore/[roadmapmetaid]/IndentedTreeWithData.tsx
@@ -124,11 +124,13 @@ const IndentedTreeWithData = ({
       .attr("font-weight", (d) => (d.depth === 0 ? 900 : 100))
       .style("font-size", (d) => getLabelFontSize(d))
       .style("font-family", "'Poppins', sans-serif")
-      .text((d) => d.data.name)
+      .text((d) => {
+        const nodeName = d.data.name;
+        return nodeName;
+      })
       .attr("fill", "#cbd5e1")
       .on("click", function (d) {
         const data = d.target.__data__;
-        console.log(data.height);
         if (!d.children && data.height === 0 && isCreator) {
           handleTitleClick(d);
         }
@@ -138,6 +140,25 @@ const IndentedTreeWithData = ({
           d3.select(this).style("cursor", "pointer");
         }
       });
+
+    // node
+    //   .filter((d) => d.height === 0) 
+    //   .append("foreignObject") 
+    //   .attr("x", (d) => d.depth * nodeSize + getTextXOffset(d, 10, 80))
+    //   .attr("y", -20)
+    //   .attr("width", 350)
+    //   .attr("height", 30)
+    //   .html(function (d) {
+    //     return `
+    //   <div class="flex items-center">
+    //     <input checked  type="checkbox" value="" class="w-4 h-4 text-green-600 bg-gray-100 border-gray-300 rounded focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+    //     <label  class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">${d.data.name}</label>
+    //   </div>
+    // `;
+    //   })
+    //   .each(function (d) {
+    //     d3.select(this).style("font-family", "'Poppins', sans-serif");
+    //   });
 
     node.append("title").text((d) =>
       d
