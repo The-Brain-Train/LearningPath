b59f6416fce4909c2deabb387b079c4cbcc9736c
Luke Williams <lukewilliams10101@gmail.com>
2023-11-22 14:25:09 +0100

    fix: fixed resources in mobile view

--- front-end/app/components/ResourcesSection.tsx
+++ front-end/app/components/ResourcesSection.tsx
@@ -1,19 +1,10 @@
 import Link from "next/link";
-import { TreeNode } from "@/app/util/types";
+import { ResourcesSectionProps, TreeNode } from "@/app/util/types";
 import { CircularProgress } from "@mui/material";
 import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
 import { addResourcesToRoadmap } from "@/app/functions/httpRequests";
 import { requestPromptOnlyResources } from "../functions/chatPreHistory";
 import { getResponseFromOpenAI } from "../functions/openAIChat";
-
-type ResourcesSectionProps = {
-  treeNode: TreeNode | null | undefined;
-  userOwnsRoadmap: boolean;
-  queriesToInvalidate: string[];
-  roadmapId: string | undefined;
-  userEmail: string | null | undefined;
-  cookiesUser: string;
-};
 
 export const ResourcesSection = (props: ResourcesSectionProps) => {
   const queryClient = useQueryClient();
@@ -27,7 +18,7 @@ export const ResourcesSection = (props: ResourcesSectionProps) => {
         const resourcesJsonData = await JSON.parse(
           response.choices[0].message.content
         );
-        const roadmap = await addResourcesToRoadmap(
+        await addResourcesToRoadmap(
           props.userEmail,
           props.roadmapId,
           resourcesJsonData,
@@ -40,59 +31,59 @@ export const ResourcesSection = (props: ResourcesSectionProps) => {
     });
 
   return (
-    <>
-      {((props.userOwnsRoadmap && props.roadmapId) ||
-        (props.treeNode && props.treeNode.resources)) && (
-        <div className="mt-10 flex flex-left">
-          <p className="text-xl text-center font-bold text-white md:text-2xl">
-            Resources to follow
-          </p>
-        </div>
-      )}
-      <div className="mt-5 mx-10">
-        {props.treeNode &&
-          props.treeNode.resources &&
-          props.treeNode.resources.map((r, index) => (
-            <table className="table-fixed w-full" key={index}>
-              <tbody>
-                <tr className="py-10">
-                  <td>{r.type}</td>
-                  <td>{r.name}</td>
-                  <td>
-                    <Link
-                      href={r.link}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded">
-                        Visit Site
-                      </button>
-                    </Link>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          ))}
-        {(!props.treeNode || !props.treeNode.resources) &&
-          !generatingResources &&
-          props.userOwnsRoadmap &&
-          props.roadmapId && (
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded"
-              onClick={() => handleAddResources()}
-            >
-              Add Resources
-            </button>
-          )}
-        {generatingResources && props.roadmapId && (
-          <>
-            <div className="font-bold text-xl text-slate-300">
-              Generating Resources
-            </div>
-            <CircularProgress />
-          </>
+    <div className="pb-12 text-white flex flex-col items-center">
+      <div className="max-w-7xl mx-2">
+        {((props.userOwnsRoadmap && props.roadmapId) ||
+          (props.treeNode && props.treeNode.resources)) && (
+          <div className="mb-3">
+            <p className="font-bold text-2xl md:text-4xl">Resources</p>
+          </div>
         )}
+        <div>
+          {props.treeNode &&
+            props.treeNode.resources &&
+            props.treeNode.resources.map((r, index) => (
+              <table className="table-fixed w-full" key={index}>
+                <tbody>
+                  <tr className="py-10 w-full">
+                    <td className="md:text-xl w-2/6">{r.type}</td>
+                    <td className="text-sm md:text-lg w-3/6">{r.name}</td>
+                    <td className="text-sm md:text-xl w-1/6">
+                      <Link
+                        href={r.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded">
+                          Visit
+                        </button>
+                      </Link>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            ))}
+          {(!props.treeNode || !props.treeNode.resources) &&
+            !generatingResources &&
+            props.userOwnsRoadmap &&
+            props.roadmapId && (
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded"
+                onClick={() => handleAddResources()}
+              >
+                Add Resources
+              </button>
+            )}
+          {generatingResources && props.roadmapId && (
+            <>
+              <div className="font-bold text-xl text-slate-300">
+                Generating Resources
+              </div>
+              <CircularProgress />
+            </>
+          )}
+        </div>
       </div>
-    </>
+    </div>
   );
 };
