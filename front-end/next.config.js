2592641526d22a1ec50f06384b8cf49ab7f3706f
Atufa Shabnam <atufa.shabnam@gmail.com>
2023-11-23 10:12:06 +0100

    fix: fixed the serializtion error in the console

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -1,16 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['lh3.googleusercontent.com', 'localhost', 'storage.googleapis.com'],
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'avatars.githubusercontent.com',
-                port: '',
-                pathname: '/u/**',
-            },
-        ],
+      domains: ['lh3.googleusercontent.com', 'localhost', 'storage.googleapis.com'],
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'avatars.githubusercontent.com',
+          port: '',
+          pathname: '/u/**',
+        },
+      ],
     },
-}
-
-module.exports = nextConfig
+    webpack: (config, { dev }) => {
+      if (!dev) {
+        config.stats = {
+          warningsFilter: /serializing big strings/,
+        };
+      }
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;
+  
