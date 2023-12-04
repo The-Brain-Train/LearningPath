cbb517dd62deec8e169f5859657ef2069542e4fe
Luke Williams <lukewilliams10101@gmail.com>
2023-12-04 10:08:22 +0100

    fix: added symbols for sigup password validation in backend

--- back-end/src/main/java/com/braintrain/backend/util/AppConstants.java
+++ back-end/src/main/java/com/braintrain/backend/util/AppConstants.java
@@ -2,5 +2,5 @@ package com.braintrain.backend.util;
 
 public class AppConstants {
     public static final String EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$";
-    public static final String PASSWORD_REGEX = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=]).{8,}$";
+    public static final String PASSWORD_REGEX = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=*!-]).{8,}$";
 }
