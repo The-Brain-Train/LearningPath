6b515db11f930660ad707efd97264bf9d25b921f
Luke Williams <lukewilliams10101@gmail.com>
2023-12-04 16:53:39 +0100

    refactor: validations modified for validation in sign up/sign in

--- front-end/app/functions/validations.ts
+++ front-end/app/functions/validations.ts
@@ -3,59 +3,47 @@ import { SignUpFormType } from "../signup/SignupForm";
 
 export const validateSignUpForm = (
   formData: SignUpFormType,
-  confirmPassword: string,
+  confirmPassword: string
 ) => {
-  checkEmptyFields(formData.name, formData.email, formData.password);
+  validateName(formData.name);
   validateEmail(formData.email);
   validatePassword(formData.password);
   validateConfirmPassword(formData.password, confirmPassword);
 };
 
 export const validateSignInForm = (formData: SignInFormType) => {
-  checkIfEmailAndPasswordAreEmpty(formData.email, formData.password);
   validateEmail(formData.email);
-}
-
-export const checkEmptyFields = (
-  name: string,
-  email: string,
-  password: string,
-) => {
-  if (name.trim().length === 0) {
-    throw new Error("Name cannot be empty.");
+  if (formData.password.trim().length === 0) {
+    throw new Error("password|Invalid password.");
   }
-  checkIfEmailAndPasswordAreEmpty(email, password);
+};
+
+export const validateName = (name: string) => {
+  if (name.trim().length === 0) {
+    throw new Error("name|Name cannot be empty.");
+  }
 };
 
 export const validateEmail = (email: string) => {
   const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-  if (!regex.test(email)) {
-    throw new Error("Invalid email format.");
+  if (!regex.test(email) || email.trim().length === 0) {
+    throw new Error("email|Invalid email.");
   }
 };
 
 export const validatePassword = (password: string) => {
   const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=*!-]).{8,}$/;
 
-  if (!regex.test(password)) {
-    throw new Error("Invalid password.");
+  if (!regex.test(password) || password.trim().length === 0) {
+    throw new Error("password|Invalid password.");
   }
 };
 
 export const validateConfirmPassword = (
   password: string,
-  confirmPassword: string,
+  confirmPassword: string
 ) => {
   if (!(password === confirmPassword)) {
-    throw new Error("Passwords don't match.");
+    throw new Error("passwordConfirmation|Passwords don't match.");
   }
 };
-
-const checkIfEmailAndPasswordAreEmpty = (email: string, password: string) => {
-  if (email.trim().length === 0) {
-    throw new Error("Email cannot be empty.");
-  }
-  if (password.trim().length === 0) {
-    throw new Error("Password cannot be empty.");
-  }
-}
