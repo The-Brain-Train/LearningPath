a832bf8b5a6ad1be00939746bf7f14e1fa09692d
Atufa Shabnam <atufa.shabnam@gmail.com>
2023-12-08 18:14:49 +0100

    fix: config

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -1,16 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['lh3.googleusercontent.com', 'localhost', 'storage.googleapis.com'],
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'avatars.githubusercontent.com',
-          port: '',
-          pathname: '/u/**',
-        },
-      ],
-    },
     webpack: (config, { dev }) => {
       if (!dev) {
         config.stats = {
