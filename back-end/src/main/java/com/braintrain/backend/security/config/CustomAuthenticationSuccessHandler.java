57412efa0c0e7b754d8cb18dd018f6551e6a26b3
Atufa Shabnam <atufa.shabnam@gmail.com>
2023-12-08 13:46:02 +0100

    fix: system.out

--- back-end/src/main/java/com/braintrain/backend/security/config/CustomAuthenticationSuccessHandler.java
+++ back-end/src/main/java/com/braintrain/backend/security/config/CustomAuthenticationSuccessHandler.java
@@ -48,6 +48,7 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
                 userRepository.save(newUser);
                 token = jwtServiceImpl.generateToken(newUser);
             }
+            System.out.println(createNewCookie(token));
             response.addCookie(createNewCookie(token));
             response.sendRedirect(websiteProperties.frontend());
         }
@@ -63,9 +64,12 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
     }
 
     private String getDomain(String url) {
+    
         if (websiteProperties.frontend().equals(url)) {
+            System.out.println(url);
             return "learning-path-pi.vercel.app";
         }
+        System.out.println("not triggered");
         return "localhost";
     }
 }
