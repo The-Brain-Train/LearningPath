4f2211a6cc80a6ef0a8f906ff4b9691e5c7c56ee
Luke Williams <lukewilliams10101@gmail.com>
2023-12-12 11:53:14 +0100

    feat: implemented to obtain token from backend from oauth log in

---
+++ front-end/app/components/JwtAuth.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+import jwtDecode from "jwt-decode";
+import { useCookies } from "react-cookie";
+
+type DecodedToken = {
+  exp: number;
+};
+
+const JwtAuth = () => {
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+  const [cookies, setCookie] = useCookies(["user"]);
+
+  const isTokenExpired = !cookies.user || checkIfTokenExpired(cookies.user);
+
+  if (token && (isTokenExpired || !cookies.user)) {
+    const tokenString = Array.isArray(token) ? token[0] : token;
+    setCookie("user", tokenString, {
+        path: "/",
+      });
+  }
+
+  function checkIfTokenExpired(token: string) {
+    const decodedToken: DecodedToken = jwtDecode(token);
+    if (!decodedToken) {
+      return true;
+    }
+    const currentTimestamp = Math.floor(Date.now() / 1000);
+    return decodedToken.exp < currentTimestamp;
+  }
+
+  return null;
+};
+
+export default JwtAuth;
