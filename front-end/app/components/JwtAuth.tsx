dc169079be149c2c07a11db285e6bf7e861688c2
Luke Williams <lukewilliams10101@gmail.com>
2023-12-13 11:30:01 +0100

    refactor: removed unused comp + commented out jwtauth for now

--- front-end/app/components/JwtAuth.tsx
+++ front-end/app/components/JwtAuth.tsx
@@ -1,47 +1,37 @@
-"use client";
-import { useRouter, useSearchParams } from "next/navigation";
-import jwtDecode from "jwt-decode";
-import { useCookies } from "react-cookie";
-import { useEffect } from "react";
+// "use client";
+// import { useRouter, useSearchParams } from "next/navigation";
+// import jwtDecode from "jwt-decode";
+// import { useCookies } from "react-cookie";
 
-type DecodedToken = {
-  exp: number;
-};
+// type DecodedToken = {
+//   exp: number;
+// };
 
-const JwtAuth = () => {
-  const searchParams = useSearchParams();
-  const token = searchParams.get("token");
-  const [cookies, setCookie] = useCookies(["user"]);
-  const router = useRouter();
+// const JwtAuth = () => {
+//   const searchParams = useSearchParams();
+//   const token = searchParams.get("token");
+//   const [cookies, setCookie] = useCookies(["user"]);
+//   const router = useRouter();
 
-  const isTokenExpired = !cookies.user || checkIfTokenExpired(cookies.user);
+//   const isTokenExpired = !cookies.user || checkIfTokenExpired(cookies.user);
 
-  if (token && (isTokenExpired || !cookies.user)) {
-    const tokenString = Array.isArray(token) ? token[0] : token;
-    setCookie("user", tokenString, {
-      path: "/",
-    });
-  }
+//   if (token && (isTokenExpired || !cookies.user)) {
+//     const tokenString = Array.isArray(token) ? token[0] : token;
+//     setCookie("user", tokenString, {
+//       path: "/",
+//     });
+//   }
 
-  function checkIfTokenExpired(token: string) {
-    const decodedToken: DecodedToken = jwtDecode(token);
-    if (!decodedToken) {
-      return true;
-    }
-    const currentTimestamp = Math.floor(Date.now() / 1000);
-    return decodedToken.exp < currentTimestamp;
-  }
+//   function checkIfTokenExpired(token: string) {
+//     const decodedToken: DecodedToken = jwtDecode(token);
+//     if (!decodedToken) {
+//       return true;
+//     }
+//     const currentTimestamp = Math.floor(Date.now() / 1000);
+//     return decodedToken.exp < currentTimestamp;
+//   }
 
-  useEffect(() => {
-    if (token && (isTokenExpired || !cookies.user)) {
-      router.push("/");
-      setTimeout(() => {
-        window.location.reload();
-      }, 500);
-    }
-  }, [token, isTokenExpired, cookies.user, router]);
+//   return null;
+// };
 
-  return null;
-};
-
-export default JwtAuth;
+// export default JwtAuth;
--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,11 +1,5 @@
 import HomePage from "./components/HomePage";
-import JwtAuth from "./components/JwtAuth";
 
 export default function Home() {
-  return (
-    <>
-      <HomePage />
-      {/* <JwtAuth /> */}
-    </>
-  );
+  return <HomePage />;
 }
