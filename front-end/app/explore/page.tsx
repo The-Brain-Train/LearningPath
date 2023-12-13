03a449aec1596097d63d3dd7c38a1cdfecd7372f
Luke Williams <lukewilliams10101@gmail.com>
2023-12-13 21:28:09 +0100

    refactor: modified page for new filter component

--- front-end/app/explore/page.tsx
+++ front-end/app/explore/page.tsx
@@ -9,28 +9,22 @@ import {
   getUserFavorites,
   getUserUpVotesDownVotes,
 } from "../functions/httpRequests";
-import TuneIcon from "@mui/icons-material/Tune";
-import { Box, Button, CircularProgress } from "@mui/material";
-import Tooltip from "@mui/material/Tooltip";
+import { Box, CircularProgress } from "@mui/material";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useCookies } from "react-cookie";
-import useMediaQuery from "@mui/material/useMediaQuery";
 import ReactPaginate from "react-paginate";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import useCurrentUserQuery from "../functions/useCurrentUserQuery";
 import { RoadmapsPage } from "./RoadmapsPage";
-import FilterShow from "./FilterShow";
 import { FilterProvider } from "./FilterProvider";
-import DesktopFilter from "./DesktopFilter";
+import ExploreFilters from "./ExploreFilters";
 
 export default function Explore() {
-  const [showFilters, setShowFilters] = useState(false);
   const itemsPerPage = 9;
   const [pageCount, setPageCount] = useState(0);
   const queryClient = useQueryClient();
   const [cookies] = useCookies(["user"]);
-  const isSmallScreen = useMediaQuery("(max-width: 768px)");
   const { currentUser, isLoading } = useCurrentUserQuery();
 
   const fetchUserFavorites = async () => {
@@ -48,15 +42,13 @@ export default function Explore() {
   };
 
   const { data: roadmaps } = useQuery(["roadmaps"], () => {
-
     let page: number = queryClient.getQueryData(["thisPage"])
       ? queryClient.getQueryData<number>(["thisPage"]) || 0
       : 0;
 
     const experienceLevel =
       queryClient.getQueryData<string>(["experienceLevel"]) || "";
-    const searchText =
-      queryClient.getQueryData<string>(["searchText"]) || "";
+    const searchText = queryClient.getQueryData<string>(["searchText"]) || "";
     const hoursFromFilter =
       queryClient.getQueryData<number>(["hoursFromFilter"]) || 0;
     const hoursToFilter =
@@ -92,7 +84,7 @@ export default function Explore() {
 
     queryClient.setQueryData<string>(["prevExperienceLevel"], experienceLevel);
     queryClient.setQueryData<string>(["prevSearchText"], searchText);
-    queryClient.setQueryData<number>(["prevHoursFromFilter"], hoursFromFilter)
+    queryClient.setQueryData<number>(["prevHoursFromFilter"], hoursFromFilter);
     queryClient.setQueryData<number>(["prevHoursToFilter"], hoursToFilter);
 
     return result;
@@ -115,10 +107,6 @@ export default function Explore() {
       enabled: !!currentUser,
     }
   );
-
-  const toggleFilters = () => {
-    setShowFilters(!showFilters);
-  };
 
   const handleSearchChange = (
     event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -151,10 +139,10 @@ export default function Explore() {
   return (
     <FilterProvider>
       <main className="main-background min-h-max flex items-center flex-col">
-        <div className="flex flex-row my-5 mx-auto md:gap-x-10">
+        <div className="flex flex-col md:flex-row my-5 md:items-center gap-3 md:gap-5">
           <Paper
             component="div"
-            className="py-2 px-4 flex w-15 max-h-12 justify-center items-center md:mt-5 w-64"
+            className="py-2 px-4 flex w-15 max-h-12 w-64"
           >
             <InputBase
               sx={{ ml: 1, flex: 1 }}
@@ -166,17 +154,8 @@ export default function Explore() {
               <SearchIcon />
             </IconButton>
           </Paper>
-          {isSmallScreen ? (
-            <Button onClick={toggleFilters}>
-              <Tooltip title="Filter">
-                <TuneIcon className="text-white" />
-              </Tooltip>
-            </Button>
-          ) : (
-            <DesktopFilter />
-          )}
+          <ExploreFilters />
         </div>
-        {showFilters && <FilterShow />}
         <ReactPaginate
           previousLabel={<ArrowBackIcon />}
           nextLabel={<ArrowForwardIcon />}
