6e34f761f74fbe566ecfdf4350e50ace9c5cd0fd
Luke Williams <lukewilliams10101@gmail.com>
2023-12-14 16:35:02 +0100

    style: change of header

--- front-end/app/create/InputForm.tsx
+++ front-end/app/create/InputForm.tsx
@@ -36,7 +36,7 @@ const InputForm = ({
 
   const capitalizeFirstLetter = (text: string) => {
     return text.charAt(0).toUpperCase() + text.slice(1);
-  };  
+  };
 
   const handleSubmit = () => {
     if (!userTopic) {
@@ -65,121 +65,123 @@ const InputForm = ({
   };
 
   return (
-    <>
-      <div className="flex flex-col items-center text-center">
-        {data === null && (
-          <div className="mt-5 max-w-xs">
-            <h1 className="flex justify-center p-5 text-white text-xl">
-              What would you want to learn?
-            </h1>
-            <div className="flex justify-center">
-              <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-xl"
-                onClick={() => {
-                  handleOpen();
-                  resetForm();
-                }}
-              >
-                Create Roadmap
-              </button>
-            </div>
-            <p className="text-white text-xs pt-5 px-4">
-              <span className="underline">Note:</span> You must have an account
-              and be signed in to save roadmaps
-            </p>
+    <div className="flex flex-col items-center text-center">
+      <h1 className="text-white text-4xl md:text-5xl mt-8 mb-3 max-w-sm md:max-w-lg font-bold text-center mx-3">
+        Create Your Roadmap
+      </h1>
+      {data === null && (
+        <div className="mt-5 max-w-xs">
+          <div className="flex justify-center">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-xl"
+              onClick={() => {
+                handleOpen();
+                resetForm();
+              }}
+            >
+              Create
+            </button>
           </div>
-        )}
-        <Modal
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box sx={modalStyle}>
+          <p className="text-white text-xs pt-5 px-4">
+            <span className="underline">Note:</span> You must have an account
+            and be signed in to save roadmaps
+          </p>
+        </div>
+      )}
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={modalStyle}>
+          <div>
+            <InputLabel
+              className="ml-3 form-control"
+              id="demo-simple-topic-autowidth-label"
+            >
+              Topic:
+            </InputLabel>
+            <TextField
+              type="text"
+              value={userTopic}
+              onChange={(e) =>
+                setUserTopic(capitalizeFirstLetter(e.target.value))
+              }
+              placeholder="Enter Topic!"
+              sx={{ m: 1, minWidth: "90%" }}
+              className="pt-0 rounded-l-md focus:outline-none focus:placeholder-gray-400 text-center placeholder-gray-60 form-control"
+            />
+            {topicError && (
+              <div className="text-red-500 ml-2">{topicError}</div>
+            )}
+            <FormControl sx={{ m: 1, minWidth: "90%" }}>
+              <InputLabel id="demo-simple-select-autowidth-label">
+                Experience Level
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-autowidth-label"
+                id="demo-simple-select-autowidth"
+                value={experience}
+                onChange={handleChange}
+                autoWidth
+                label="Experience Level"
+                className="border-white"
+              >
+                <MenuItem value={"beginner"}>Beginner</MenuItem>
+                <MenuItem value={"intermediate"}>Intermediate</MenuItem>
+                <MenuItem value={"expert"}>Expert</MenuItem>
+              </Select>
+            </FormControl>
+            {experienceError && (
+              <div className="text-red-500 ml-2">{experienceError}</div>
+            )}
             <div>
               <InputLabel
-                className="ml-3 form-control"
-                id="demo-simple-topic-autowidth-label"
+                className="mt-5 pb-2 text-sm font-black"
+                id="demo-simple-box-autowidth-label"
               >
-                Topic:
+                Total hours
               </InputLabel>
-              <TextField
-                type="text"
-                value={userTopic}
-                onChange={(e) => setUserTopic(capitalizeFirstLetter(e.target.value))}
-                placeholder="Enter Topic!"
-                sx={{ m: 1, minWidth: "90%" }}
-                className="pt-0 rounded-l-md focus:outline-none focus:placeholder-gray-400 text-center placeholder-gray-60 form-control"
-              />
-              {topicError && <div className="text-red-500 ml-2">{topicError}</div>}
-              <FormControl sx={{ m: 1, minWidth: "90%" }}>
-                <InputLabel id="demo-simple-select-autowidth-label">
-                  Experience Level
-                </InputLabel>
-                <Select
-                  labelId="demo-simple-select-autowidth-label"
-                  id="demo-simple-select-autowidth"
-                  value={experience}
-                  onChange={handleChange}
-                  autoWidth
-                  label="Experience Level"
-                  className="border-white"
-                >
-                  <MenuItem value={"beginner"}>Beginner</MenuItem>
-                  <MenuItem value={"intermediate"}>Intermediate</MenuItem>
-                  <MenuItem value={"expert"}>Expert</MenuItem>
-                </Select>
-              </FormControl>
-              {experienceError && (
-                <div className="text-red-500 ml-2">{experienceError}</div>
-              )}
-              <div>
-                <InputLabel
-                  className="mt-5 pb-2 text-sm font-black"
-                  id="demo-simple-box-autowidth-label"
-                >
-                  Total hours
-                </InputLabel>
-                <Box sx={{ minWidth: "80%" }}>
-                  <Slider
-                    aria-label="Hours"
-                    value={sliderValue}
-                    onChange={(event, newValue) =>
-                      setSliderValue(newValue as number)
-                    }
-                    defaultValue={30}
-                    getAriaValueText={valuetext}
-                    valueLabelDisplay="auto"
-                    step={10}
-                    marks
-                    min={0}
-                    max={500}
-                  />
-                </Box>
-              </div>
-              <div className="mt-2 flex justify-start flex-row">
-                <InputLabel className="mt-2 pb-2 text-sm font-black">
-                  Add resources
-                </InputLabel>
-                <Switch
-                  checked={toggleValue}
-                  onChange={() => setToggleValue(!toggleValue)}
-                  inputProps={{ 'aria-label': 'Toggle' }}
+              <Box sx={{ minWidth: "80%" }}>
+                <Slider
+                  aria-label="Hours"
+                  value={sliderValue}
+                  onChange={(event, newValue) =>
+                    setSliderValue(newValue as number)
+                  }
+                  defaultValue={30}
+                  getAriaValueText={valuetext}
+                  valueLabelDisplay="auto"
+                  step={10}
+                  marks
+                  min={0}
+                  max={500}
                 />
-              </div>
-              <div className="flex justify-center">
-                <button
-                  onClick={handleSubmit}
-                  className="w-3/6 bg-blue-500 hover:bg-blue-700 mt-5 text-white font-bold py-2 px-4 border border-blue-700 rounded"
-                >
-                  Create
-                </button>
-              </div>
+              </Box>
             </div>
-          </Box>
-        </Modal>
-      </div>
-    </>
+            <div className="mt-2 flex justify-start flex-row">
+              <InputLabel className="mt-2 pb-2 text-sm font-black">
+                Add resources
+              </InputLabel>
+              <Switch
+                checked={toggleValue}
+                onChange={() => setToggleValue(!toggleValue)}
+                inputProps={{ "aria-label": "Toggle" }}
+              />
+            </div>
+            <div className="flex justify-center">
+              <button
+                onClick={handleSubmit}
+                className="w-3/6 bg-blue-500 hover:bg-blue-700 mt-5 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+              >
+                Create
+              </button>
+            </div>
+          </div>
+        </Box>
+      </Modal>
+    </div>
   );
 };
 
