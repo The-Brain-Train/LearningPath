43aa4b7dacd6348faff78bf8b4d16712e1003622
Luke Williams <lukewilliams10101@gmail.com>
2023-12-15 09:23:26 +0100

    fix: fixed error for using selected

--- front-end/app/explore/ExploreFilters.tsx
+++ front-end/app/explore/ExploreFilters.tsx
@@ -2,7 +2,7 @@ import { useFilterContext } from "./FilterProvider";
 import { useQueryClient } from "@tanstack/react-query";
 import { useState, useEffect } from "react";
 
-export default function FilterShow() {
+export default function ExploreFilters() {
   const queryClient = useQueryClient();
   const filterContext = useFilterContext();
   const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth > 768);
@@ -82,7 +82,7 @@ export default function FilterShow() {
               id="experienceLevel_disabled"
               className="font-medium bg-gray-50 border border-gray-300 text-gray-900 rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             >
-              <option value="" selected>
+              <option value="">
                 All
               </option>
               <option value="beginner">Beginner</option>
