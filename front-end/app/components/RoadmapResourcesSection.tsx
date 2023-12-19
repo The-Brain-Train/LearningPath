e47eb988c4ac609e7672f8bd7458a26d3c7c9915
vijanipiyawardana <vijanipiyawardana@gmail.com>
2023-12-19 22:16:44 +0100

    feat: added props for resource suggestion and fixed minor issues

--- front-end/app/components/RoadmapResourcesSection.tsx
+++ front-end/app/components/RoadmapResourcesSection.tsx
@@ -14,7 +14,7 @@ type RoadmapResourcesSectionProps = {
   treeNode: TreeNode | null;
   userOwnsRoadmap: boolean;
   queriesToInvalidate: string[];
-  roadmapId: string | undefined;
+  roadmapMetaId: string | undefined;
   userEmail: string | null | undefined;
   cookiesUser: string;
 };
@@ -57,7 +57,7 @@ export const RoadmapResourcesSection = (
         );
         await addResourcesToRoadmap(
           props.userEmail,
-          props.roadmapId,
+          props.roadmapMetaId,
           resourcesJsonData,
           props.cookiesUser
         );
@@ -75,7 +75,7 @@ export const RoadmapResourcesSection = (
   return (
     <>
       <div className="pb-12 text-white flex flex-col">
-        {((props.userOwnsRoadmap && props.roadmapId) ||
+        {((props.userOwnsRoadmap && props.roadmapMetaId) ||
           (props.treeNode && props.treeNode.resources)) && (
             <div className="mt-10 flex flex-left">
               <p className="text-xl text-center font-bold text-white md:text-3xl pl-4">
@@ -99,13 +99,12 @@ export const RoadmapResourcesSection = (
                     </td>
 
                     <td className="md:w-5/6 hover:text-blue-500 hover:font-bold align-middle py-4 underline">
-                      <Link
-                        href={r.link}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        {r.name}
-                      </Link>
+                      {r.link &&
+                        <Link
+                          href={r.link} target="_blank" rel="noopener noreferrer">
+                          {r.name}
+                        </Link>
+                      }
                     </td>
                   </tr>
                 </tbody>
@@ -119,16 +118,18 @@ export const RoadmapResourcesSection = (
                 <li className="font-bold list-disc"> {r.type} </li>
                 <li className="pl-2 pb-1">
                   <span className="mr-2">{r.name}</span>
-                  <Link href={r.link} target="_blank" rel="noopener noreferrer">
-                    <OpenInNew />
-                  </Link>
+                  {r.link &&
+                    <Link href={r.link} target="_blank" rel="noopener noreferrer">
+                      <OpenInNew />
+                    </Link>
+                  }
                 </li>
               </ul>
             ))}
           {(!props.treeNode || !props.treeNode.resources) &&
             !generatingResources &&
             props.userOwnsRoadmap &&
-            props.roadmapId && (
+            props.roadmapMetaId && (
               <button
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded"
                 onClick={() => handleAddResources()}
@@ -136,7 +137,7 @@ export const RoadmapResourcesSection = (
                 Add Resources
               </button>
             )}
-          {generatingResources && props.roadmapId && (
+          {generatingResources && props.roadmapMetaId && (
             <div className="flex flex-row items-center">
               <div className="text-left font-bold text-xl text-slate-300 mr-2">
                 Generating Resources
@@ -147,7 +148,7 @@ export const RoadmapResourcesSection = (
           {(props.treeNode && !props.treeNode.resources) &&
             !generatingResources &&
             !props.userOwnsRoadmap &&
-            props.roadmapId && (
+            props.roadmapMetaId && (
               <button
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded"
                 onClick={() => handleSuggestResources()}
@@ -156,6 +157,9 @@ export const RoadmapResourcesSection = (
               </button>
             )}
           <SuggestResourceForm
+            roadmapMetaId={props.roadmapMetaId}
+            userEmail={props.userEmail}
+            cookiesUser={props.cookiesUser}
             setSuggestResourceData={setSuggestResourceData}
             resetForm={resetForm}
           />
