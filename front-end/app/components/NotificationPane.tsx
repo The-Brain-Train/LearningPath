b2500474185a9b3b9facd9607f82d9df89ffa5bc
vijanipiyawardana <vijanipiyawardana@gmail.com>
2023-12-22 16:34:22 +0100

    feat: added function to parse notification body json to display resourse details on dialog box

--- front-end/app/components/NotificationPane.tsx
+++ front-end/app/components/NotificationPane.tsx
@@ -7,7 +7,7 @@ import {
   TextField
 } from "@mui/material";
 import NotificationsIcon from '@mui/icons-material/Notifications';
-import { User } from '../util/types';
+import { ResourceType, User } from '../util/types';
 import { MouseEvent, useState } from "react";
 import { useQueryClient, useQuery, useMutation } from "@tanstack/react-query";
 import { modalStyle } from "../create/createModalStyle";
@@ -47,6 +47,7 @@ const NotificationPane = (props: NotificationPaneType) => {
 
   const [open, setOpen] = useState(false);
   const [curNotificationOption, setCurNotificationOption] = useState(-1);
+  const [curNotificationResourse, setCurNotificationResourse] = useState<ResourceType | null>(null);
   const handleClose = () => setOpen(false);
   const [currentNotification, setCurrentNotification] = useState<NotificationType | null>(null);
   const queryClient = useQueryClient();
@@ -71,20 +72,13 @@ const NotificationPane = (props: NotificationPaneType) => {
     }
   );
 
-  const showHideModel = () => {
-    const open =
-      queryClient.getQueryData<boolean>(["showSuggestResourceNotification"]) || false;
-    setOpen(open);
-    queryClient.setQueryData(["showSuggestResourceNotification"], false);
-    return open;
-  };
-
   const handleNotificationClick = (notification: NotificationType) => {
     markNotificationAsRead(
       notification.id,
       props.cookieUserToken
     );
     setCurrentNotification(notification);
+    setCurNotificationResourse(parseResourseJson(notification));
     setOpen(true);
   };
 
@@ -119,20 +113,28 @@ const NotificationPane = (props: NotificationPaneType) => {
   };
 
   const { mutateAsync: mutateNotificationDeleteClick } =
-  useMutation(
-    async ({ notification }: { notification: NotificationType }): Promise<any> => {
-      return deleteNotification(notification.id, props.cookieUserToken);
-    },
-    {
-      onSuccess: () => {
-        queryClient.invalidateQueries(["allNotifications"]);
+    useMutation(
+      async ({ notification }: { notification: NotificationType }): Promise<any> => {
+        return deleteNotification(notification.id, props.cookieUserToken);
       },
-    });
+      {
+        onSuccess: () => {
+          queryClient.invalidateQueries(["allNotifications"]);
+        },
+      });
 
   const handleNotificationDeleteClick = (e: MouseEvent, notification: NotificationType) => {
     e.stopPropagation();
     mutateNotificationDeleteClick({ notification: notification });
     setCurNotificationOption(-1);
+  };
+
+  const showHideModel = () => {
+    const open =
+      queryClient.getQueryData<boolean>(["showSuggestResourceNotification"]) || false;
+    setOpen(open);
+    queryClient.setQueryData(["showSuggestResourceNotification"], false);
+    return open;
   };
 
   const handleResourceSuggestionConfirm = () => {
@@ -156,109 +158,125 @@ const NotificationPane = (props: NotificationPaneType) => {
     handleClose();
   };
 
-  return (
-    <>
-      <div className="relative" >
-        {/* <div className="flex flex-row items-center"> */}
-        <NotificationsIcon className="my-5" />
-        {/* <p className="pl-2">Notifications</p> */}
-        {/* </div> */}
-        {props.notificationsVisible &&
-          <div onBlur={handleClose} className="absolute w-96 right-0 bg-white rounded-md shadow-xl z-10 overflow-y-auto max-h-80">
-            {allNotifications &&
-              allNotifications.map((n, index) => (
-                <div
-                  // onClick={() => handleNotificationClick(n)}
-                  key={index}
-                  className="block px-4 py-2 h-18 line-clamp-3 text-left whitespace-normal text-gray-700 hover:bg-gray-300"
-                >
-                  <div className="flex flex-row">
-                    <div className="items-center pr-2 py-2">
-                      <p className="text-base">💡</p>
-                    </div>
-                    <div
-                      onClick={() => handleNotificationClick(n)}
-                    >
-                      <p className={`text-sm ${n.isRead ? 'text-gray-500' : 'text-gray-800'}`}>
-                        <span className="font-bold">{n.senderName}</span>
-                        {` ${n.message} `}
-                        <span className="font-bold">{n.roadmapName}</span>
-                      </p>
-                    </div>
-                    <div className="flex flex-col items-end items-center">
-                      <MoreHorizIcon
-                        className="relative hover:bg-slate-400 hover:rounded-full"
-                        onClick={(e) => handleNotificationMoreButtonClick(e, n, index)}
-                      />
-                      {curNotificationOption === index &&
-                        <div
-                          className="absolute w-32 right-0 z-10 bg-white my-6 shadow-md text-wrap mr-5"
-                          onMouseLeave={() => handleNotificationMoreMouseLeaveClick(index)}>
-                          <ul className="text-sm">
-                            <li
-                              className="hover:bg-slate-300 p-1"
-                              onClick={(e) => handleNotificationStatusClick(e, n)}
-                            >
-                              {n.isRead ? 'Mark as unread' : 'Mark as read'}
-                            </li>
-                            <li
-                              className="hover:bg-slate-300 p-1"
-                              onClick={(e) => handleNotificationDeleteClick(e, n)}
-                            >
-                              Delete
-                            </li>
-                          </ul>
-                        </div>
-                      }
-                    </div>
+  const parseResourseJson = (notification: NotificationType) => {
+    const bodyString = notification.body;
+    const parsedBody = JSON.parse(bodyString);
+    const resourcesArray: ResourceType[] = parsedBody.resources;
+    const resource = resourcesArray[0];
+    return resource;
+  };
+
+return (
+  <>
+    <div className="relative" >
+      {/* <div className="flex flex-row items-center"> */}
+      <NotificationsIcon className="my-5" />
+      {/* <p className="pl-2">Notifications</p> */}
+      {/* </div> */}
+      {props.notificationsVisible &&
+        <div onBlur={handleClose} className="absolute w-96 right-0 bg-white rounded-md shadow-xl z-10 overflow-y-auto max-h-80">
+          {allNotifications &&
+            allNotifications.map((n, index) => (
+              <div
+                // onClick={() => handleNotificationClick(n)}
+                key={index}
+                className="block px-4 py-2 h-18 line-clamp-3 text-left whitespace-normal text-gray-700 hover:bg-gray-300"
+              >
+                <div className="flex flex-row">
+                  <div className="items-center pr-2 py-2">
+                    <p className="text-base">💡</p>
                   </div>
-                  <div className="pl-7 pr-1 pt-2 flex flex-row justify-between">
-                    <p className={`text-left text-xs ${n.isRead ? 'text-gray-500' : 'text-sky-700'}`}>
-                      {n.timeDiffMessage}
+                  <div
+                    onClick={() => handleNotificationClick(n)}
+                  >
+                    <p className={`text-sm ${n.isRead ? 'text-gray-500' : 'text-gray-800'}`}>
+                      <span className="font-bold">{n.senderName}</span>
+                      {` ${n.message} `}
+                      <span className="font-bold">{n.roadmapName}</span>
                     </p>
-                    {!n.isRead && <CircleIcon color="primary" fontSize="inherit" className="pb-1" />}
+                  </div>
+                  <div className="flex flex-col items-end items-center">
+                    <MoreHorizIcon
+                      className="relative hover:bg-slate-400 hover:rounded-full"
+                      onClick={(e) => handleNotificationMoreButtonClick(e, n, index)}
+                    />
+                    {curNotificationOption === index &&
+                      <div
+                        className="absolute w-32 right-0 z-10 bg-white my-6 shadow-md text-wrap mr-5"
+                        onMouseLeave={() => handleNotificationMoreMouseLeaveClick(index)}>
+                        <ul className="text-sm">
+                          <li
+                            className="hover:bg-slate-300 p-1"
+                            onClick={(e) => handleNotificationStatusClick(e, n)}
+                          >
+                            {n.isRead ? 'Mark as unread' : 'Mark as read'}
+                          </li>
+                          <li
+                            className="hover:bg-slate-300 p-1"
+                            onClick={(e) => handleNotificationDeleteClick(e, n)}
+                          >
+                            Delete
+                          </li>
+                        </ul>
+                      </div>
+                    }
                   </div>
                 </div>
-              ))
-            }
-          </div>}
-      </div>
+                <div className="pl-7 pr-1 pt-2 flex flex-row justify-between">
+                  <p className={`text-left text-xs ${n.isRead ? 'text-gray-500' : 'text-sky-700'}`}>
+                    {n.timeDiffMessage}
+                  </p>
+                  {!n.isRead && <CircleIcon color="primary" fontSize="inherit" className="pb-1" />}
+                </div>
+              </div>
+            ))
+          }
+        </div>}
+    </div>
 
-      {currentNotification &&
-        currentNotification.type === "ROADMAP_RESOURCE_SUGGESTED" &&
-        <Modal
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box sx={modalStyle}>
-            <div>
-              {/* <FormLabel className="text-white">
+    {currentNotification &&
+      currentNotification.type === "ROADMAP_RESOURCE_SUGGESTED" &&
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={modalStyle}>
+          <div>
+            {/* <FormLabel className="text-white">
                 {currentNotification.body}
               </FormLabel> */}
-              <span>
-                {currentNotification.body}
-              </span>
-              <div className="flex flex-row justify-between">
-                <button
-                  onClick={handleResourceSuggestionConfirm}
-                  className="w-2/6 mx-4 bg-blue-500 hover:bg-blue-700 mt-5 text-white font-bold py-2 px-4 border border-blue-700 rounded"
-                >
-                  Confirm
-                </button>
-                <button
-                  onClick={handleResourceSuggestionReject}
-                  className="w-2/6 mx-4 bg-blue-500 hover:bg-blue-700 mt-5 text-white font-bold py-2 px-4 border border-blue-700 rounded"
-                >
-                  Reject
-                </button>
-              </div>
+            <p className="mb-10">
+              Add suggested resource into your roadmap&nbsp;
+              <span className="font-bold">
+                {currentNotification.roadmapName}
+              </span>.
+            </p>
+            <div>
+              <p>Name: {curNotificationResourse?.name}</p>
+              <p>Type: {curNotificationResourse?.type}</p>
+              <p>Link: {curNotificationResourse?.link}</p>
             </div>
-          </Box>
-        </Modal>}
-    </>
-  )
+            <div className="flex flex-row justify-between">
+              <button
+                onClick={handleResourceSuggestionConfirm}
+                className="w-2/6 mx-4 bg-blue-500 hover:bg-blue-700 mt-5 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+              >
+                Confirm
+              </button>
+              <button
+                onClick={handleResourceSuggestionReject}
+                className="w-2/6 mx-4 bg-blue-500 hover:bg-blue-700 mt-5 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+              >
+                Reject
+              </button>
+            </div>
+          </div>
+        </Box>
+      </Modal>}
+  </>
+)
 }
 
 export default NotificationPane
