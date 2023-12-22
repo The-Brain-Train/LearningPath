bd8859aa891dbd1e59d4509905a8c94f526182ce
vijanipiyawardana <vijanipiyawardana@gmail.com>
2023-12-22 20:37:56 +0100

    style: UI design for ResourceSuggestionView

--- front-end/app/components/ResourceSuggestionView.tsx
+++ front-end/app/components/ResourceSuggestionView.tsx
@@ -1,18 +1,15 @@
 import {
     Box,
-    FormLabel,
-    FormControl,
-    MenuItem,
-    Modal,
-    TextField
+    Modal
 } from "@mui/material";
-import { modalStyle } from "../create/createModalStyle";
+import { modalStyle } from "../util/modalStyle";
 import { NotificationType } from "../components/NotificationPane";
 import { ResourceType } from "../util/types";
 import {
     addResourcesToRoadmap,
     markNotificationAsProcessed
 } from "../functions/httpRequests";
+import Link from "next/link";
 
 type ResourceSuggestionViewProps = {
     notification: NotificationType;
@@ -62,41 +59,66 @@ const ResourceSuggestionView = (props: ResourceSuggestionViewProps) => {
         >
             <Box sx={modalStyle}>
                 <div>
-                    <p className="mb-10">
-                        Add suggested resource into your roadmap&nbsp;
+                    <p className="mt-2 mb-10 text-center">
+                        Suggested resource into your roadmap&nbsp;
                         <span className="font-bold">
                             {props.notification.roadmapName}
                         </span>.
                     </p>
-                    <div>
-                        <p>Name: {curNotificationResourse?.name}</p>
-                        <p>Type: {curNotificationResourse?.type}</p>
-                        <p>Link: {curNotificationResourse?.link}</p>
-                    </div>
+                    <div className="border-2 border-dotted border-inherit rounded">
+                        <p className="text-center italic my-4">
+                            {curNotificationResourse?.type}
+                        </p>
 
+                        {curNotificationResourse?.link ? (
+                            <Link
+                                href={curNotificationResourse?.link}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <p className="text-center text-xl mb-4 hover:text-blue-500 hover:font-bold hover:underline">
+                                    {curNotificationResourse?.name}
+                                </p>
+                            </Link>
+                        ) : (
+                            <p className="text-center text-xl mb-4">
+                                {curNotificationResourse?.name}
+                            </p>
+                        )}
+                    </div>
                     {!props.notification.isProcessed ? (
-                        <div className="flex flex-row justify-between">
-                            <button
-                                onClick={handleResourceSuggestionConfirm}
-                                className="w-2/6 mx-4 bg-blue-500 hover:bg-blue-700 mt-5 text-white font-bold py-2 px-4 border border-blue-700 rounded"
-                            >
-                                Confirm
-                            </button>
-                            <button
-                                onClick={handleResourceSuggestionReject}
-                                className="w-2/6 mx-4 bg-blue-500 hover:bg-blue-700 mt-5 text-white font-bold py-2 px-4 border border-blue-700 rounded"
-                            >
-                                Reject
-                            </button>
+                        <div className="flex flex-col mt-10">
+                            <p className="text-center">Do you accept this resource suggestion?</p>
+                            <div className="flex flex-row justify-between">
+                                <button
+                                    onClick={handleResourceSuggestionConfirm}
+                                    className="w-2/6 mx-4 bg-blue-500 hover:bg-blue-700 mt-5 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+                                >
+                                    Accept
+                                </button>
+                                <button
+                                    onClick={handleResourceSuggestionReject}
+                                    className="w-2/6 mx-4 bg-blue-500 hover:bg-blue-700 mt-5 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+                                >
+                                    Reject
+                                </button>
+                            </div>
                         </div>) : (
-                        <div>
-                            <p>This resourse is already added.</p>
-                            <button
-                                onClick={handleResourceSuggestionReject}
-                                className="w-2/6 mx-4 bg-blue-500 hover:bg-blue-700 mt-5 text-white font-bold py-2 px-4 border border-blue-700 rounded"
-                            >
-                                OK
-                            </button>
+                        <div className="flex flex-col mt-10">
+                            <p className="text-center">
+                                This resourse is already added into roadmap&nbsp;
+                                <span className="font-bold">
+                                    {props.notification.roadmapName}
+                                </span>.
+                            </p>
+                            <div className="flex flex-row justify-center">
+                                <button
+                                    onClick={handleResourceSuggestionReject}
+                                    className="w-2/6 mx-4 bg-blue-500 hover:bg-blue-700 mt-5 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+                                >
+                                    OK
+                                </button>
+                            </div>
                         </div>
                     )}
                 </div>
