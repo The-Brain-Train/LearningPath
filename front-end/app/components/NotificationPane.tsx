c167d0a2f1df60870e6d96f962c037de65677857
Atufa Shabnam <atufa.shabnam@gmail.com>
2023-12-27 00:01:06 +0100

    fix: reformated the notification pane

--- front-end/app/components/NotificationPane.tsx
+++ front-end/app/components/NotificationPane.tsx
@@ -1,17 +1,17 @@
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import { User } from '../util/types';
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import { User } from "../util/types";
 import { MouseEvent, useEffect, useState } from "react";
 import { useQueryClient, useQuery, useMutation } from "@tanstack/react-query";
 import {
   getAllNotificationsOfUser,
   markNotificationAsRead,
   markNotificationAsUnRead,
-  deleteNotification
+  deleteNotification,
 } from "../functions/httpRequests";
-import CircleIcon from '@mui/icons-material/Circle';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import CircleIcon from "@mui/icons-material/Circle";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import ResourceSuggestionView from "./ResourceSuggestionView";
-import { useRouter } from 'next/navigation'
+import { useRouter } from "next/navigation";
 
 type NotificationPaneType = {
   currentUser: User | null | undefined;
@@ -37,21 +37,25 @@ export type NotificationType = {
 };
 
 const NotificationPane = (props: NotificationPaneType) => {
-
-  const [openSuggestedResourseBox, setOpenSuggestedResourseBox] = useState(false);
+  const [openSuggestedResourseBox, setOpenSuggestedResourseBox] =
+    useState(false);
   const [curNotificationOption, setCurNotificationOption] = useState(-1);
-  const [currentNotification, setCurrentNotification] = useState<NotificationType | null>(null);
+  const [currentNotification, setCurrentNotification] =
+    useState<NotificationType | null>(null);
   const [unreadCount, setUnreadCount] = useState(0);
-  const handleCloseSuggestedResourseBox = () => setOpenSuggestedResourseBox(false);
+  const handleCloseSuggestedResourseBox = () =>
+    setOpenSuggestedResourseBox(false);
   const queryClient = useQueryClient();
-  const router = useRouter()
+  const router = useRouter();
 
   const { data: allNotifications, isSuccess: allNotificationsRecieved } =
     useQuery<NotificationType[]>(
       ["allNotifications"],
-      () => getAllNotificationsOfUser(
-        props.currentUser?.email as string,
-        props.cookieUserToken),
+      () =>
+        getAllNotificationsOfUser(
+          props.currentUser?.email as string,
+          props.cookieUserToken
+        ),
       {
         enabled: !!props.currentUser,
       }
@@ -59,27 +63,27 @@ const NotificationPane = (props: NotificationPaneType) => {
 
   useEffect(() => {
     if (allNotificationsRecieved && allNotifications.length > 0) {
-      const count = allNotifications.filter(n => !n.isRead).length;
+      const count = allNotifications.filter((n) => !n.isRead).length;
       setUnreadCount(count);
     } else {
       setUnreadCount(0);
     }
   }, [allNotifications]);
 
-  const { mutateAsync: mutateNotificationClick } =
-    useMutation(
-      async ({ notification }: { notification: NotificationType }):
-        Promise<any> => {
-        return markNotificationAsRead(
-          notification.id,
-          props.cookieUserToken
-        );
+  const { mutateAsync: mutateNotificationClick } = useMutation(
+    async ({
+      notification,
+    }: {
+      notification: NotificationType;
+    }): Promise<any> => {
+      return markNotificationAsRead(notification.id, props.cookieUserToken);
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(["allNotifications"]);
       },
-      {
-        onSuccess: () => {
-          queryClient.invalidateQueries(["allNotifications"]);
-        },
-      });
+    }
+  );
 
   const handleNotificationClick = (notification: NotificationType) => {
     mutateNotificationClick({ notification: notification });
@@ -93,10 +97,9 @@ const NotificationPane = (props: NotificationPaneType) => {
       case "ROADMAP_UNFAVORITED":
       case "ROADMAP_UPVOTED":
       case "ROADMAP_DOWNVOTED": {
-        router.push(
-          `/explore/${notification.roadmapMetaId}`,
-          { scroll: false }
-        );
+        router.push(`/explore/${notification.roadmapMetaId}`, {
+          scroll: false,
+        });
         break;
       }
       default: {
@@ -105,8 +108,11 @@ const NotificationPane = (props: NotificationPaneType) => {
     }
   };
 
-  const handleNotificationMoreButtonClick = (e: MouseEvent,
-    notification: NotificationType, index: number) => {
+  const handleNotificationMoreButtonClick = (
+    e: MouseEvent,
+    notification: NotificationType,
+    index: number
+  ) => {
     e.stopPropagation();
     setCurNotificationOption(index);
   };
@@ -115,60 +121,73 @@ const NotificationPane = (props: NotificationPaneType) => {
     setCurNotificationOption(-1);
   };
 
-  const { mutateAsync: mutateNotificationStatusClick } =
-    useMutation(
-      async ({ notification }: { notification: NotificationType }):
-        Promise<any> => {
-        return notification.isRead
-          ? markNotificationAsUnRead(notification.id, props.cookieUserToken)
-          : markNotificationAsRead(notification.id, props.cookieUserToken);
+  const { mutateAsync: mutateNotificationStatusClick } = useMutation(
+    async ({
+      notification,
+    }: {
+      notification: NotificationType;
+    }): Promise<any> => {
+      return notification.isRead
+        ? markNotificationAsUnRead(notification.id, props.cookieUserToken)
+        : markNotificationAsRead(notification.id, props.cookieUserToken);
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(["allNotifications"]);
       },
-      {
-        onSuccess: () => {
-          queryClient.invalidateQueries(["allNotifications"]);
-        },
-      });
+    }
+  );
 
-  const handleNotificationStatusClick =
-    (e: MouseEvent, notification: NotificationType) => {
-      e.stopPropagation();
-      mutateNotificationStatusClick({ notification: notification });
-      setCurNotificationOption(-1);
-    };
+  const handleNotificationStatusClick = (
+    e: MouseEvent,
+    notification: NotificationType
+  ) => {
+    e.stopPropagation();
+    mutateNotificationStatusClick({ notification: notification });
+    setCurNotificationOption(-1);
+  };
 
-  const { mutateAsync: mutateNotificationDeleteClick } =
-    useMutation(
-      async ({ notification }: { notification: NotificationType }):
-        Promise<any> => {
-        return deleteNotification(notification.id, props.cookieUserToken);
+  const { mutateAsync: mutateNotificationDeleteClick } = useMutation(
+    async ({
+      notification,
+    }: {
+      notification: NotificationType;
+    }): Promise<any> => {
+      return deleteNotification(notification.id, props.cookieUserToken);
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(["allNotifications"]);
       },
-      {
-        onSuccess: () => {
-          queryClient.invalidateQueries(["allNotifications"]);
-        },
-      });
+    }
+  );
 
-  const handleNotificationDeleteClick =
-    (e: MouseEvent, notification: NotificationType) => {
-      e.stopPropagation();
-      mutateNotificationDeleteClick({ notification: notification });
-      setCurNotificationOption(-1);
-    };
+  const handleNotificationDeleteClick = (
+    e: MouseEvent,
+    notification: NotificationType
+  ) => {
+    e.stopPropagation();
+    mutateNotificationDeleteClick({ notification: notification });
+    setCurNotificationOption(-1);
+  };
 
   return (
     <>
-      <div className="relative mx-1 md:mx-6" >
-        {unreadCount > 0 &&
+      <div className="relative mx-1 md:mx-6">
+        {unreadCount > 0 && (
           <div className="absolute top-8 left-4 bg-red-500 rounded-full w-4 h-4 text-center text-xs font-semibold text-white">
             {unreadCount}
           </div>
-        }
+        )}
         <NotificationsIcon
           className="my-5 cursor-pointer hover:bg-gray-100"
           onMouseDown={props.onIconClick}
         />
-        {props.notificationsVisible &&
-          <div onBlur={handleCloseSuggestedResourseBox} className="absolute w-72 md:w-96 right-0 bg-white rounded-md shadow-xl z-10 overflow-y-auto max-h-80">
+        {props.notificationsVisible && (
+          <div
+            onBlur={handleCloseSuggestedResourseBox}
+            className="absolute w-72 md:w-96 right-0 bg-white rounded-md shadow-xl z-10 overflow-y-auto max-h-80"
+          >
             {allNotifications &&
               allNotifications.map((n, index) => (
                 <div
@@ -179,61 +198,75 @@ const NotificationPane = (props: NotificationPaneType) => {
                     <div className="items-center pr-2 py-2">
                       <p className="text-base">💡</p>
                     </div>
-                    <div
-                      onClick={() => handleNotificationClick(n)}
-                    >
-                      <p className={`text-sm mr-2 md:w-64 ${n.isRead ? 'text-gray-500' : 'text-gray-800'}`}>
+                    <div onClick={() => handleNotificationClick(n)}>
+                      <p
+                        className={`text-sm mr-2 md:w-64 ${
+                          n.isRead ? "text-gray-500" : "text-gray-800"
+                        }`}
+                      >
                         <span className="font-bold">{n.senderName}</span>
                         {` ${n.message} `}
                         <span className="font-bold">{n.roadmapName}</span>
                       </p>
                     </div>
-                    <div className="flex flex-col items-end items-center">
+                    <div className="flex flex-col items-end">
                       <MoreHorizIcon
                         className="relative hover:bg-slate-400 hover:rounded-full"
-                        onClick={(e) => handleNotificationMoreButtonClick(e, n, index)}
+                        onClick={(e) =>
+                          handleNotificationMoreButtonClick(e, n, index)
+                        }
                       />
-                      {curNotificationOption === index &&
+                      {curNotificationOption === index && (
                         <div
                           className="absolute w-32 right-0 z-10 bg-white my-6 shadow-md text-wrap mr-5"
-                          onMouseLeave={() => handleNotificationMoreMouseLeaveClick(index)}
+                          onMouseLeave={() =>
+                            handleNotificationMoreMouseLeaveClick(index)
+                          }
                         >
                           <ul className="text-sm">
                             <li
                               className="hover:bg-slate-300 p-1"
-                              onClick={(e) => handleNotificationStatusClick(e, n)}
+                              onClick={(e) =>
+                                handleNotificationStatusClick(e, n)
+                              }
                             >
-                              {n.isRead ? 'Mark as unread' : 'Mark as read'}
+                              {n.isRead ? "Mark as unread" : "Mark as read"}
                             </li>
                             <li
                               className="hover:bg-slate-300 p-1"
-                              onClick={(e) => handleNotificationDeleteClick(e, n)}
+                              onClick={(e) =>
+                                handleNotificationDeleteClick(e, n)
+                              }
                             >
                               Delete
                             </li>
                           </ul>
                         </div>
-                      }
+                      )}
                     </div>
                   </div>
                   <div className="pl-7 md:pl-10 pr-1 pt-2 flex flex-row justify-between">
-                    <p className={`text-left text-xs ${n.isRead ? 'text-gray-500' : 'text-sky-700'}`}>
+                    <p
+                      className={`text-left text-xs ${
+                        n.isRead ? "text-gray-500" : "text-sky-700"
+                      }`}
+                    >
                       {n.timeDiffMessage}
                     </p>
-                    {!n.isRead &&
+                    {!n.isRead && (
                       <CircleIcon
                         color="primary"
                         fontSize="inherit"
                         className="pb-1"
                       />
-                    }
+                    )}
                   </div>
                 </div>
-              ))
-            }
-          </div>}
+              ))}
+          </div>
+        )}
       </div>
-      {currentNotification && props.currentUser &&
+      {currentNotification && props.currentUser && (
         <ResourceSuggestionView
           notification={currentNotification}
           open={openSuggestedResourseBox}
@@ -241,9 +274,9 @@ const NotificationPane = (props: NotificationPaneType) => {
           userEmail={props.currentUser.email as string}
           cookieUserToken={props.cookieUserToken}
         />
-      }
+      )}
     </>
-  )
-}
+  );
+};
 
-export default NotificationPane
+export default NotificationPane;
